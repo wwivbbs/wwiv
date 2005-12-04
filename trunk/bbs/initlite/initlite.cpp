b496b556cccf19eab00bbeca49da19cf6efc8950
atani <atani@efe8ed7e-8846-41e4-9041-cb610019eb92>
2005-12-04 16:08:27 +0000

    Adding status.users to Status edit screen.

--- trunk/bbs/initlite/initlite.cpp
+++ trunk/bbs/initlite/initlite.cpp
@@ -348,6 +348,7 @@ int EditStatus()
 		clear();
 		mvprintw(0, 0, "Init Lite version 5.0");
 		mvprintw(2, 0, "1) Num Calls:  %d", status.callernum1);
+		mvprintw(3, 0, "2) Num Users:  %d", status.users);
 		mvprintw(8, 0, "(Q=Quit) Enter Command : ");
 		int key = getch();
 		key = toupper(key);
@@ -361,6 +362,16 @@ int EditStatus()
 			if (strlen(szLine)>0)
 			{
 				status.callernum1 = atoi(szLine);
+			}
+			break;
+		case '2':
+			mvprintw(10, 0, "Enter new number of users:");
+			move(11, 0); 
+			getnstr(szLine, 81);
+			mvprintw(15, 0, "Line entered = '%s'", szLine);
+			if (strlen(szLine)>0)
+			{
+				status.users = atoi(szLine);
 			}
 			break;
 		case 'Q': 
