d29409d4135455f1eedaaa331cbcaf6b559d98cf
rushfan <rushfan@efe8ed7e-8846-41e4-9041-cb610019eb92>
2005-12-06 16:23:19 +0000

    Initial Revision

---
+++ trunk/bbs/WTextFile.cpp
@@ -0,0 +1,68 @@
+/**************************************************************************/
+/*                                                                        */
+/*                              WWIV Version 5.0x                         */
+/*             Copyright (C)1998-2005, WWIV Software Services             */
+/*                                                                        */
+/*    Licensed  under the  Apache License, Version  2.0 (the "License");  */
+/*    you may not use this  file  except in compliance with the License.  */
+/*    You may obtain a copy of the License at                             */
+/*                                                                        */
+/*                http://www.apache.org/licenses/LICENSE-2.0              */
+/*                                                                        */
+/*    Unless  required  by  applicable  law  or agreed to  in  writing,   */
+/*    software  distributed  under  the  License  is  distributed on an   */
+/*    "AS IS"  BASIS, WITHOUT  WARRANTIES  OR  CONDITIONS OF ANY  KIND,   */
+/*    either  express  or implied.  See  the  License for  the specific   */
+/*    language governing permissions and limitations under the License.   */
+/*                                                                        */
+/**************************************************************************/
+#include "WTextFile.h"
+
+FILE *fsh_open(const char *path, char *mode);
+
+
+WTextFile::WTextFile( const char* pszFileName, const char* pszFileMode )
+{
+    m_hFile = NULL;
+    strcpy( m_szFileName, pszFileName );
+    strcpy( m_szFileName, pszFileMode );
+    m_hFile = fsh_open( m_szFileName, m_szFileMode);
+}
+
+bool WTextFile::Close()
+{
+    if ( m_hFile != NULL )
+    {
+        fclose(m_hFile);
+        m_hFile = NULL;
+        return true;
+    }
+    return false;
+}
+
+
+int WTextFile::WriteFormatted( const char *pszFormatText,... )
+{
+    va_list ap;
+    char szBuffer[ 4096 ];
+
+    va_start( ap, pszFormatText );
+    vsnprintf( szBuffer, sizeof( szBuffer ), pszFormatText, ap );
+    va_end( ap );
+    return Write( szBuffer );
+}
+
+
+bool WTextFile::ReadLine( std::string &buffer )
+{
+    char szBuffer[4096];
+    char *pszBuffer = fgets(szBuffer, sizeof(szBuffer), m_hFile);
+    buffer = szBuffer;
+    return ( pszBuffer != NULL );
+}
+
+
+WTextFile::~WTextFile()
+{
+    Close();
+}
---
+++ trunk/bbs/WTextFile.h
@@ -0,0 +1,56 @@
+#ifndef __INCLUDED_WTEXTFILE_H__
+#define __INCLUDED_WTEXTFILE_H__
+/**************************************************************************/
+/*                                                                        */
+/*                              WWIV Version 5.0x                         */
+/*             Copyright (C)1998-2005, WWIV Software Services             */
+/*                                                                        */
+/*    Licensed  under the  Apache License, Version  2.0 (the "License");  */
+/*    you may not use this  file  except in compliance with the License.  */
+/*    You may obtain a copy of the License at                             */
+/*                                                                        */
+/*                http://www.apache.org/licenses/LICENSE-2.0              */
+/*                                                                        */
+/*    Unless  required  by  applicable  law  or agreed to  in  writing,   */
+/*    software  distributed  under  the  License  is  distributed on an   */
+/*    "AS IS"  BASIS, WITHOUT  WARRANTIES  OR  CONDITIONS OF ANY  KIND,   */
+/*    either  express  or implied.  See  the  License for  the specific   */
+/*    language governing permissions and limitations under the License.   */
+/*                                                                        */
+/**************************************************************************/
+
+
+#if defined( _WIN32 )
+#define _CRT_SECURE_NO_DEPRECATE
+#define WIN32_LEAN_AND_MEAN
+#include <cstdio>
+#include <cstring>
+#include <string>
+#include <windows.h>
+#endif 
+
+class WTextFile
+{
+
+public:
+    WTextFile( const char* pszFileName, const char* pszFileMode );
+    operator FILE*() const { return m_hFile; }
+    bool Close();
+    bool IsOpen() { return m_hFile != NULL; }
+    int Write( const char *pszText ) { return fputs( pszText, m_hFile ); }
+    int WriteFormatted( const char *pszFormatText, ... );
+    bool ReadLine( char *pszBuffer, int nBufferSize ) { return (fgets(pszBuffer, nBufferSize, m_hFile) != NULL) ? true : false; }
+    bool ReadLine( std::string &buffer );
+    long GetPosition() { return ftell(m_hFile); }
+public:
+    ~WTextFile();
+
+private:
+    char    m_szFileName[ MAX_PATH + 1 ];
+    char    m_szFileMode[ 10 ];
+    FILE*   m_hFile;
+};
+
+
+
+#endif // __INCLUDED_WTEXTFILE_H__
