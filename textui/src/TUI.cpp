64e3c4c1bab1ac5fe9dd181da0e26a84f4df74c5
atani <atanisoft@users.noreply.github.com>
2007-06-05 18:10:45 +0000

    new InputBox control

--- textui/src/TUI.cpp
+++ textui/src/TUI.cpp
@@ -9,6 +9,7 @@
 #include "MessageBox.h"
 #include "Menu.h"
 #include "MenuBar.h"
+#include "InputBox.h"
 
 class ExitCommand : public UICommand
 {
@@ -25,13 +26,26 @@ class InfoCommand : public UICommand
     {
         UIDesktop *desktop = UIDesktop::GetDesktop();
         UIMessageBox infoWindow( desktop, 10, 40, 5, 20, true );
-        infoWindow.AddText( "About WWIV Configuration Utility", true );
+        infoWindow.AddText( "About Text UI", true );
         infoWindow.AddText( "" );
-        infoWindow.AddText( "Copyright (c) 2006 WWIV Software Services", true );
+        infoWindow.AddText( "Copyright (c) 2007 WWIV Software Services", true );
         infoWindow.AddText( "All Rights Reserved", true );
         infoWindow.AddText( "http://wwiv.sourceforge.net", true );
-        infoWindow.SetTitle( "About WWIV" );
+        infoWindow.SetTitle( "About TUI" );
         infoWindow.Run();
+        return true;
+    }
+};
+
+class InputCommand : public UICommand
+{
+    virtual bool Execute()
+    {
+        UIDesktop *desktop = UIDesktop::GetDesktop();
+		UIInputBox inputBox(desktop, 5, 40, 5, 20, "Enter Name:", "Input Demo");
+		inputBox.Run();
+		std::string result = inputBox.GetText();
+		desktop->WriteCentered(desktop->GetHeight() - 3, result);
         return true;
     }
 };
@@ -39,14 +53,14 @@ class InfoCommand : public UICommand
 void CreateMenus( UIMenuBar* menuBar )
 {
     UIMenu *fileMenu = new UIMenu( "File" );
-    fileMenu->Add( new UIMenuItem("Switches", NULL ) );
-    fileMenu->Add( new UIMenuItem("Info", new InfoCommand ) );
+    fileMenu->Add( new UIMenuItem("Input Test", new InputCommand ) );
+    fileMenu->Add( new UIMenuItem("MessageBox Test", new InfoCommand ) );
     fileMenu->Add( new UIMenuItem ( "Exit", new ExitCommand ) );
     menuBar->AddMenu( fileMenu );
-    menuBar->AddMenu( new UIMenu( "System" ) );
-    menuBar->AddMenu( new UIMenu( "Options" ) );
-    menuBar->AddMenu( new UIMenu( "Modem" ) );
-    menuBar->AddMenu( new UIMenu( "Manager" ) );
+    //menuBar->AddMenu( new UIMenu( "System" ) );
+    //menuBar->AddMenu( new UIMenu( "Options" ) );
+    //menuBar->AddMenu( new UIMenu( "Modem" ) );
+    //menuBar->AddMenu( new UIMenu( "Manager" ) );
 }
 
 
@@ -54,8 +68,8 @@ int main( int argc, char* argv[] )
 {
     UIDesktop* desktop = UIDesktop::InitializeDesktop( true, true );
     UISubWindow *window = new UISubWindow( desktop, 6, 50, 10, 15, UIColors::BACKGROUND_TEXT_COLOR, false );
-    window->WriteCentered( 1, " WWIV 5.0 Configuration Utility; 5.0.62 " );
-    window->WriteCentered( 3, " Copyright (c) 2006 WWIV Software Services. " );
+    window->WriteCentered( 1, " Text UI Demo " );
+    window->WriteCentered( 3, " Copyright (c) 2007 WWIV Software Services. " );
     window->WriteCentered( 5, " All Rights Reserved. " );
 
     CreateMenus( desktop->GetMenuBar() );
