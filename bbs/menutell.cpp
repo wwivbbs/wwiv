6e70c6203621b5256579ee1d9e8607e57f094cff
wwivbbs <wwivsoftwareservices@users.noreply.github.com>
2014-02-18 03:18:15 +0000

    Updated copyright date in trunk *.cpp and *.h

--- bbs/menutell.cpp
+++ bbs/menutell.cpp
@@ -1,19 +1,21 @@
-/****************************************************************************/
-/*                                                                          */
-/*                             WWIV Version 5.0x                            */
-/*            Copyright (C) 1998-2004 by WWIV Software Services             */
-/*                       Copyright (c) ????, ????????????                   */
-/*                           Used with permission                           */
-/*                                                                          */
-/*      Distribution or publication of this source code, it's individual    */
-/*       components, or a compiled version thereof, whether modified or     */
-/*        unmodified, without PRIOR, WRITTEN APPROVAL of WWIV Software      */
-/*        Services is expressly prohibited.  Distribution of compiled       */
-/*            versions of WWIV is restricted to copies compiled by          */
-/*           WWIV Software Services.  Violators will be procecuted!         */
-/*                                                                          */
-/****************************************************************************/
-//#include "port.h"
+/**************************************************************************/
+/*                                                                        */
+/*                              WWIV Version 5.0x                         */
+/*             Copyright (C)1998-2014, WWIV Software Services             */
+/*                                                                        */
+/*    Licensed  under the  Apache License, Version  2.0 (the "License");  */
+/*    you may not use this  file  except in compliance with the License.  */
+/*    You may obtain a copy of the License at                             */
+/*                                                                        */
+/*                http://www.apache.org/licenses/LICENSE-2.0              */
+/*                                                                        */
+/*    Unless  required  by  applicable  law  or agreed to  in  writing,   */
+/*    software  distributed  under  the  License  is  distributed on an   */
+/*    "AS IS"  BASIS, WITHOUT  WARRANTIES  OR  CONDITIONS OF ANY  KIND,   */
+/*    either  express  or implied.  See  the  License for  the specific   */
+/*    language governing permissions and limitations under the License.   */
+/*                                                                        */
+/**************************************************************************/
 
 #include <conio.h>
 #include <stdarg.h>
