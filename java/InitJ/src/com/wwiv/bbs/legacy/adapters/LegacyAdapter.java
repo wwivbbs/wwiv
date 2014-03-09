46fa89638e919cc715370d0e5fa778297c656423
Rushfan <wwiv@users.noreply.github.com>
2014-03-09 05:58:02 +0000

    remove unused java code

--- java/InitJ/src/com/wwiv/bbs/legacy/adapters/LegacyAdapter.java
+++
@@ -1,9 +0,0 @@
-package com.wwiv.bbs.legacy.adapters;
-
-import com.wwiv.bbs.legacy.model.LegacyConfiguration;
-import com.wwiv.bbs.model.SystemConfiguration;
-
-public interface LegacyAdapter<L,M> {
-    M fromLegacy(L legacy);
-    L toLegacy(M config);
-}
