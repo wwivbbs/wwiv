661f9fe4cb3eaaa9257815a48dfd75e1068588e0
rushfan <rushfan@efe8ed7e-8846-41e4-9041-cb610019eb92>
2014-03-09 05:58:02 +0000

    remove unused java code

--- trunk/java/InitJ/src/com/wwiv/bbs/init/ui/ModelBoundPanel.java
+++
@@ -1,9 +0,0 @@
-package com.wwiv.bbs.init.ui;
-
-import javax.swing.JComponent;
-
-public interface ModelBoundPanel<T> {
-    JComponent getGUI();
-    T viewToModel(T model);
-    void modelToView(T model);
-}
