e1dd10e40b43ca652f4ae14817b5d7befaa2429c
Rushfan <wwiv@users.noreply.github.com>
2014-04-24 04:22:15 +0000

    separate bbs into a .lib and .exe.  Add first unit test for wfile

---
+++ bbs/bbs_main.cpp
@@ -0,0 +1,5 @@
+int bbsmain( int argc, char *argv[] );
+
+int main( int argc, char *argv[] ) {
+    return bbsmain(argc, argv);
+}
---
+++ bbs_test/wfile_test.cpp
@@ -0,0 +1,45 @@
+#include "gtest/gtest.h"
+#include <string>
+#include <io.h>
+
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#endif
+
+#include "platform/WFile.h"
+
+static char *dir_template = "fnXXXXXX";
+
+using std::cout;
+using std::string;
+
+class FileTest : public testing::Test {
+ public:
+    string TempDir() {
+        char local_dir_template[_MAX_PATH];
+        char temp_path[_MAX_PATH];
+        GetTempPath(_MAX_PATH, temp_path);
+        sprintf(local_dir_template, "%s%s", temp_path, dir_template);
+        char *result = _mktemp(local_dir_template);
+        if (!CreateDirectory(result, NULL)) {
+            *result = '\0';
+        }
+        return std::string(result);
+    }
+};
+
+TEST_F(FileTest, DoesNotExist) {
+    string tmp = TempDir();
+    GTEST_ASSERT_NE("", tmp);
+    WFile dne(tmp, "doesnotexist");
+    ASSERT_FALSE(dne.Exists());
+}
+
+TEST_F(FileTest, Exists) {
+    string tmp = TempDir();
+    GTEST_ASSERT_NE("", tmp);
+    WFile dne(tmp, "newdir");
+    ASSERT_EQ(0, _mkdir(dne.GetFullPathName().c_str()));
+    ASSERT_TRUE(dne.Exists());
+}
