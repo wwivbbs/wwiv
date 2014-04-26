9463ab79b454a2a0a6f81fe54277d738d3ff06b7
Rushfan <wwiv@users.noreply.github.com>
2014-04-26 20:12:10 +0000

    fix file tests on osx (and probably linux too)

--- bbs_test/wfile_test.cpp
+++ bbs_test/wfile_test.cpp
@@ -1,15 +1,15 @@
 #include "gtest/gtest.h"
+#include "platform/wfile.h"
+
+#include <stdio.h>
 #include <string>
-//#include <io.h>
 
 #ifdef _WIN32
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
+#else
+#include <sys/stat.h>
 #endif
-
-#include "platform/WFile.h"
-
-static char *dir_template = "fnXXXXXX";
 
 using std::cout;
 using std::string;
@@ -17,14 +17,22 @@ using std::string;
 class FileTest : public testing::Test {
  public:
     string TempDir() {
+#ifdef _WIN32
         char local_dir_template[_MAX_PATH];
         char temp_path[_MAX_PATH];
         GetTempPath(_MAX_PATH, temp_path);
+	static char *dir_template = "fnXXXXXX";
         sprintf(local_dir_template, "%s%s", temp_path, dir_template);
         char *result = _mktemp(local_dir_template);
         if (!CreateDirectory(result, NULL)) {
             *result = '\0';
         }
+#else
+        char local_dir_template[MAX_PATH];
+	static const char kTemplate[] = "/tmp/fileXXXXXX";
+	strcpy(local_dir_template, kTemplate);
+	char *result = mkdtemp(local_dir_template);
+#endif
         return std::string(result);
     }
 };
@@ -40,6 +48,10 @@ TEST_F(FileTest, Exists) {
     string tmp = TempDir();
     GTEST_ASSERT_NE("", tmp);
     WFile dne(tmp, "newdir");
+#ifdef _WIN32
     ASSERT_EQ(0, _mkdir(dne.GetFullPathName().c_str()));
+#else
+    ASSERT_EQ(0, mkdir(dne.GetFullPathName().c_str(), 0777));
+#endif  // _WIN32
     ASSERT_TRUE(dne.Exists());
 }
