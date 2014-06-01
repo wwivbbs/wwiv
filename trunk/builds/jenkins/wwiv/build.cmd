7fb47c18fb70dbec64d86d28dd8723a90d727def
rushfan <rushfan@efe8ed7e-8846-41e4-9041-cb610019eb92>
2014-06-01 23:22:01 +0000

    a few more cleanups to the build script, use @rem vs rem, use RELEASE_ZIP consistently and fully specify destination in all copy commands

--- trunk/builds/jenkins/wwiv/build.cmd
+++ trunk/builds/jenkins/wwiv/build.cmd
@@ -1,19 +1,18 @@
-rem ****************************************************************************
-rem WWIV 5.0 Build Script.
-rem
-rem Required Variables:
-rem WORKSPACE - wwiv-svn root directory
-rem BUILD_NUMBER - Jenkins Build number (arbitrary and monotonically increasing)
-rem SVN_REVISION - subversion build number.
-rem TEXT_TRANSFORM - path to text transform tool from visual studio
-rem
-rem Installed Software:
-rem   7-Zip [C:\Program Files\7-Zip\7z.exe]
-rem   VS 2013 [C:\Program Files (x86)\Microsoft Visual Studio 12.0]
-rem   msbuild [in PATH, set by vcvarsall.bat]
-rem   
-rem 
-rem ****************************************************************************
+@rem **************************************************************************
+@rem WWIV 5.0 Build Script.
+@rem
+@rem Required Variables:
+@rem WORKSPACE - wwiv-svn root directory
+@rem BUILD_NUMBER - Jenkins Build number
+@rem SVN_REVISION - subversion build number
+@rem TEXT_TRANSFORM - path to text transform tool from visual studio
+@rem
+@rem Installed Software:
+@rem   7-Zip [C:\Program Files\7-Zip\7z.exe]
+@rem   VS 2013 [C:\Program Files (x86)\Microsoft Visual Studio 12.0]
+@rem   msbuild [in PATH, set by vcvarsall.bat]
+@rem 
+@rem **************************************************************************
 
 @if exist "%ProgramFiles(x86)%\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" (
   call "%ProgramFiles(x86)%\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
@@ -27,7 +26,7 @@ set ZIP_EXE="C:\Program Files\7-Zip\7z.exe"
 set RELEASE_ZIP=%WORKSPACE%\archives\wwiv-build-%SVN_REVISION%-%BUILD_NUMBER%.zip
 echo Workspace: %WORKSPACE%         
 echo Revision:  %SVN_REVISION%
-echo  Archive:  %RELEASE_ZIP%
+echo Archive:   %RELEASE_ZIP%
 
 cd %WORKSPACE%\bbs
 %TEXT_TRANSFORM% -a !!version!%SVN_REVISION% version.template
@@ -54,21 +53,25 @@ if not exist %WORKSPACE%\archives (
    mkdir %WORKSPACE%\archives
 )
 del /q %WORKSPACE%\archives
-copy %WORKSPACE%\bbs\Release\bbs.exe %WORKSPACE%\release\
+
+
+echo Copying files to staging directory.
+
+copy %WORKSPACE%\bbs\Release\bbs.exe %WORKSPACE%\release\bbs.exe
 copy %WORKSPACE%\bbs\readme.txt %WORKSPACE%\release\readme-bbs.txt
 copy %WORKSPACE%\bbs\whatsnew.txt %WORKSPACE%\release\whatsnew.txt
-copy %WORKSPACE%\WWIV5TelnetServer\WWIV5TelnetServer\bin\release\WWIV5TelnetServer.exe %WORKSPACE%\release\
-copy %WORKSPACE%\init\Release\init.exe %WORKSPACE%\release\
-copy %WORKSPACE%\fix\Release\fix.exe %WORKSPACE%\release\
+copy %WORKSPACE%\WWIV5TelnetServer\WWIV5TelnetServer\bin\release\WWIV5TelnetServer.exe %WORKSPACE%\release\WWIV5TelnetServer.exe
+copy %WORKSPACE%\init\Release\init.exe %WORKSPACE%\release\init.exe
+copy %WORKSPACE%\fix\Release\fix.exe %WORKSPACE%\release\fix.exe
 
 echo Build URL %BUILD_URL% > release\build.nfo
 echo Subversion Build: %SVN_REVISION% >> release\build.nfo
 
 cd %WORKSPACE%\release
-%ZIP_EXE% a -tzip -y %WORKSPACE%\archives\wwiv-build-%SVN_REVISION%-%BUILD_NUMBER%.zip *
+%ZIP_EXE% a -tzip -y %RELEASE_ZIP%
 
 echo Archive File: %RELEASE_ZIP%
 echo Archive contents:
 %ZIP_EXE% l %RELEASE_ZIP%
-cd %WORKSPACE%\
+
 
