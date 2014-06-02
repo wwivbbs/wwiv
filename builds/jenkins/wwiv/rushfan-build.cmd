cee3bd8a56c3d854545f053e956d637b5a4698d9
Rushfan <wwiv@users.noreply.github.com>
2014-06-02 05:32:08 +0000

    example of how to build locally

---
+++ builds/jenkins/wwiv/rushfan-build.cmd
@@ -0,0 +1,8 @@
+SET TEXT_TRANSFORM="C:\Program Files\Common Files\microsoft shared\TextTemplating\12.0\TextTransform.exe"
+SET WORKSPACE=W:
+SET BUILD_NUMBER=0
+SET SVN_REVISION=0
+pushd %WORKSPACE%
+call %WORKSPACE%\builds\jenkins\wwiv\build.cmd
+popd
+
