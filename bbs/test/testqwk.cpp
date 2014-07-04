3bafe16b7527724be27acbc1dd65c1c5ddc44feb
Rushfan <wwiv@users.noreply.github.com>
2014-07-04 22:29:00 +0000

    fix sizeof qwk structure, it should be 128 not 132 bytes, this lets the packet work in multimail if you manually put it in there

---
+++ bbs/test/testqwk.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+#include <iostream>
+#include "../qwk.h"
+
+using std::cout;
+using std::endl;
+
+int main(char** argv) {
+  cout << "qwk_record=" << sizeof(qwk_record) << endl;
+  cout << "qwk_index=" << sizeof(qwk_index) << endl;
+}
+
