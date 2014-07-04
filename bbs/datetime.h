0b9a06813a497eef628c3b4056a731bf45939bc6
Rushfan <wwiv@users.noreply.github.com>
2014-07-04 21:35:27 +0000

    move datetime out of platform since it is common, also made some basic unit tests for it

---
+++ bbs/datetime.h
@@ -0,0 +1,40 @@
+/**************************************************************************/
+/*                                                                        */
+/*                              WWIV Version 5.0x                         */
+/*             Copyright (C)1998-2014, WWIV Software Services             */
+/*                                                                        */
+/*    Licensed  under the  Apache License, Version  2.0 (the "License");  */
+/*    you may not use this  file  except in compliance with the License.  */
+/*    You may obtain a copy of the License at                             */
+/*                                                                        */
+/*                http://www.apache.org/licenses/LICENSE-2.0              */
+/*                                                                        */
+/*    Unless  required  by  applicable  law  or agreed to  in  writing,   */
+/*    software  distributed  under  the  License  is  distributed on an   */
+/*    "AS IS"  BASIS, WITHOUT  WARRANTIES  OR  CONDITIONS OF ANY  KIND,   */
+/*    either  express  or implied.  See  the  License for  the specific   */
+/*    language governing permissions and limitations under the License.   */
+/*                                                                        */
+/**************************************************************************/
+#ifndef __INCLUDED_DATETIME_H__
+#define __INCLUDED_DATETIME_H__
+
+#include <string>
+
+char *dateFromTimeTForLog(time_t t);
+char *dateFromTimeT(time_t t);
+char *date();
+char *fulldate();
+char *times();
+time_t date_to_daten(const char *datet);
+void filedate(const char *fpath, char *rtn);
+double timer();
+long timer1();
+bool isleap(int nYear);
+int dow();
+char *ctim(double d);
+std::string ctim2(double d);
+int years_old(int nMonth, int nDay, int nYear);
+
+
+#endif // __INCLUDED_DATETIME_H__
---
+++ bbs_test/datetime_test.cpp
@@ -0,0 +1,46 @@
+/**************************************************************************/
+/*                                                                        */
+/*                              WWIV Version 5.0x                         */
+/*                Copyright (C)2014, WWIV Software Services               */
+/*                                                                        */
+/*    Licensed  under the  Apache License, Version  2.0 (the "License");  */
+/*    you may not use this  file  except in compliance with the License.  */
+/*    You may obtain a copy of the License at                             */
+/*                                                                        */
+/*                http://www.apache.org/licenses/LICENSE-2.0              */
+/*                                                                        */
+/*    Unless  required  by  applicable  law  or agreed to  in  writing,   */
+/*    software  distributed  under  the  License  is  distributed on an   */
+/*    "AS IS"  BASIS, WITHOUT  WARRANTIES  OR  CONDITIONS OF ANY  KIND,   */
+/*    either  express  or implied.  See  the  License for  the specific   */
+/*    language governing permissions and limitations under the License.   */
+/*                                                                        */
+/**************************************************************************/
+#include "gtest/gtest.h"
+
+#include <ctime>
+#include "datetime.h"
+
+static const time_t t20140704 = 1404457200; // 1404507982;
+
+
+TEST(DateTime, DateFromTimeTForLog) {
+  ASSERT_STREQ("140704", dateFromTimeTForLog(t20140704));
+}
+
+TEST(DateTime, DateFromTimeT) {
+  ASSERT_STREQ("07/04/14", dateFromTimeT(t20140704));
+}
+
+TEST(DateTime, date_to_daten) {
+  ASSERT_EQ(t20140704, date_to_daten("07/04/14"));
+}
+
+TEST(DateTime, isleap) {
+  ASSERT_TRUE(isleap(2000));
+  ASSERT_TRUE(isleap(2004));
+  ASSERT_TRUE(isleap(2008));
+  ASSERT_FALSE(isleap(2014));
+  ASSERT_FALSE(isleap(2100));
+}
+
