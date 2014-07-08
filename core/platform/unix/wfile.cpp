8190071d380714bc0da6ed5f5758fbb9cce877e8
Rushfan <wwiv@users.noreply.github.com>
2014-07-08 03:41:17 +0000

    no _strerror on unix

--- core/platform/unix/wfile.cpp
+++ core/platform/unix/wfile.cpp
@@ -202,7 +202,7 @@ bool WFile::Open(int nFileMode, int nShareMode, int nPermissions) {
 
   if (m_hFile == -1) {
     std::cout << "error opening file: " << m_szFileName << "; error: " << strerror(errno) << std::endl;
-    this->m_errorText = _strerror("_sopen");
+    this->m_errorText = strerror(errno);
   }
 
   return m_bOpen;
