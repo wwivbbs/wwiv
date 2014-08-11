602b852c37daaf9c7dc473bff0ff73d525b76f35
rushfan <rushfan@efe8ed7e-8846-41e4-9041-cb610019eb92>
2014-08-11 14:55:01 +0000

    add newline before exit message

--- trunk/WWIV5TelnetServer/WWIV5TelnetServer/MainForm.Designer.cs
+++ trunk/WWIV5TelnetServer/WWIV5TelnetServer/MainForm.Designer.cs
@@ -11,6 +11,7 @@
         /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2213:DisposableFieldsShouldBeDisposed", MessageId = "server")]
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
