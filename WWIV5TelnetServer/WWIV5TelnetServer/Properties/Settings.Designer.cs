77a7923c76c79ec0194aa46da71b1165a8537e08
Rushfan <wwiv@users.noreply.github.com>
2014-09-04 04:36:33 +0000

    default beginday event to true

--- WWIV5TelnetServer/WWIV5TelnetServer/Properties/Settings.Designer.cs
+++ WWIV5TelnetServer/WWIV5TelnetServer/Properties/Settings.Designer.cs
@@ -133,7 +133,7 @@ namespace WWIV5TelnetServer.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool useBegindayEvent {
             get {
                 return ((bool)(this["useBegindayEvent"]));
