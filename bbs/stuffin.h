cd6ff8f0585d1197226abb3457709431d9522f34
Rushfan <wwiv@users.noreply.github.com>
2014-09-15 05:07:55 +0000

    added missing files

---
+++ bbs/stuffin.h
@@ -0,0 +1,29 @@
+/**************************************************************************/
+/*                                                                        */
+/*                              WWIV Version 5.0x                         */
+/*             Copyright (C)1998-2014, WWIV Software Services             */
+/*                                                                        */
+/*    Licensed  under the  Apache License, Version  2.0 (the "License");  */
+/*    you may not use this  file  except in compliance with the License.  */
+/*    You may obtain a copy of the License at                             */
+/*                                                                        */
+/*                http://www.apache.org/licenses/LICENSE-2.0              */
+/*                                                                        */
+/*    Unless  required  by  applicable  law  or agreed to  in  writing,   */
+/*    software  distributed  under  the  License  is  distributed on an   */
+/*    "AS IS"  BASIS, WITHOUT  WARRANTIES  OR  CONDITIONS OF ANY  KIND,   */
+/*    either  express  or implied.  See  the  License for  the specific   */
+/*    language governing permissions and limitations under the License.   */
+/*                                                                        */
+/**************************************************************************/
+#ifndef __INCLUDED_STUFFIN_H__
+#define __INCLUDED_STUFFIN_H__
+
+#include <string>
+
+const std::string stuff_in(
+    const std::string& commandline, const std::string& arg1,
+    const std::string& arg2, const std::string& arg3,
+    const std::string& arg4, const std::string& arg5);
+
+#endif  // __INCLUDED_STUFFIN_H__
