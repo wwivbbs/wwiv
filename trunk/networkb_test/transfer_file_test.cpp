647fa0c821efe37a2339846e2b3986430960cb82
rushfan <rushfan@efe8ed7e-8846-41e4-9041-cb610019eb92>
2014-11-28 03:24:06 +0000

    tests file wfile_transfer_file

--- trunk/networkb_test/transfer_file_test.cpp
+++ trunk/networkb_test/transfer_file_test.cpp
@@ -2,6 +2,7 @@
 #include "core/strings.h"
 #include "core_test/file_helper.h"
 #include "networkb/transfer_file.h"
+#include "networkb/wfile_transfer_file.h"
 
 #include <chrono>
 #include <cstdint>
@@ -11,17 +12,24 @@ using std::chrono::system_clock;
 using std::chrono::time_point;
 
 using std::string;
+using std::unique_ptr;
 using namespace wwiv::net;
 using wwiv::strings::StringPrintf;
 
 class TransferFileTest : public testing::Test {
 public:
-  TransferFileTest() : filename("test1"), contents("ASDF"), now(system_clock::now()), file(filename, contents, now) {}
+  TransferFileTest() 
+    : filename("test1"), contents("ASDF"), now(system_clock::now()), file(filename, contents, now) {
+    full_filename = file_helper_.CreateTempFile(filename, contents);
+  }
 
-  time_point<system_clock> now;
-  string contents;
-  string filename;
+  const time_point<system_clock> now;
+  const string contents;
+  const string filename;
   InMemoryTransferFile file;
+  string full_filename;
+private:
+  FileHelper file_helper_;
 };
 
 TEST_F(TransferFileTest, AsPacketData) {
@@ -59,5 +67,33 @@ TEST_F(TransferFileTest, GetChunk) {
 
   // Goes past the end.
   EXPECT_FALSE(file.GetChunk(chunk, contents.size() - 1, 2));
+}
+
+TEST_F(TransferFileTest, WFileTest) {
+  WFileTransferFile wfile_file(filename, unique_ptr<WFile>(new WFile(full_filename)));
+  EXPECT_EQ(filename, wfile_file.filename());
+  EXPECT_EQ(contents.size(), wfile_file.file_size());
+
+  char chunk[100];
+
+  // Partial file.
+  memset(chunk, 0, 100);
+  ASSERT_TRUE(wfile_file.GetChunk(chunk, 0, 1));
+  EXPECT_EQ('A', chunk[0]);
+
+  // Entire wfile_file.
+  memset(chunk, 0, 100);
+  ASSERT_TRUE(wfile_file.GetChunk(chunk, 0, contents.size()));
+  EXPECT_EQ('A', chunk[0]);
+  EXPECT_EQ('S', chunk[1]);
+  EXPECT_EQ('D', chunk[2]);
+  EXPECT_EQ('F', chunk[3]);
+
+  // Goes past the end.
+  memset(chunk, 0, 100);
+  EXPECT_FALSE(wfile_file.GetChunk(chunk, 0, contents.size() + 1));
+
+  // Goes past the end.
+  EXPECT_FALSE(wfile_file.GetChunk(chunk, contents.size() - 1, 2));
 
 }
