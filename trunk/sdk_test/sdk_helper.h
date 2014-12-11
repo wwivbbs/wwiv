2b0b98f0fe56a16b3e05c907d38b72acafce36cb
rushfan <rushfan@efe8ed7e-8846-41e4-9041-cb610019eb92>
2014-12-11 15:09:06 +0000

    start of tests for sdk module, just stubs now

--- trunk/sdk_test/sdk_helper.h
+++ trunk/sdk_test/sdk_helper.h
@@ -1,7 +1,7 @@
 /**************************************************************************/
 /*                                                                        */
-/*                              WWIV Version 5.0x                         */
-/*             Copyright (C)2014, WWIV Software Services                  */
+/*                          WWIV Version 5.0x                             */
+/*               Copyright (C)2014, WWIV Software Services                */
 /*                                                                        */
 /*    Licensed  under the  Apache License, Version  2.0 (the "License");  */
 /*    you may not use this  file  except in compliance with the License.  */
@@ -14,47 +14,23 @@
 /*    "AS IS"  BASIS, WITHOUT  WARRANTIES  OR  CONDITIONS OF ANY  KIND,   */
 /*    either  express  or implied.  See  the  License for  the specific   */
 /*    language governing permissions and limitations under the License.   */
-/*                                                                        */
 /**************************************************************************/
-#include "gtest/gtest.h"
+#ifndef __INCLUDED_SDK_TEST_SDK_HELPER_H__
+#define __INCLUDED_SDK_TEST_SDK_HELPER_H__
 
-#include <iostream>
-#include <memory>
 #include <string>
 
-#include "core/file.h"
-#include "core/strings.h"
 #include "core_test/file_helper.h"
-#include "sdk/config.h"
-#include "sdk/networks.h"
-#include "sdk_test/sdk_helper.h"
 
-using std::cout;
-using std::endl;
-using std::string;
-
-using namespace wwiv::sdk;
-using namespace wwiv::strings;
-
-// TODO(rushfan): These tests don't work yet - just testing locally
-// for now. Need to create a tree under the tempdir containing a 
-// stub BBS.
-class ConfigTest : public testing::Test {
+class SdkHelper {
 public:
-  SdkHelper helper;
+  SdkHelper();
+  ~SdkHelper();
+
+  std::string CreatePath(const std::string& name);
+  FileHelper files_;
+  std::string root_;
+  std::string data_;
 };
 
-TEST_F(ConfigTest, Helper) {
-  ASSERT_TRUE(ends_with(helper.root_, "bbs")) << helper.root_;
-}
-
-TEST_F(ConfigTest, Config) {
-  const string saved_dir = File::current_directory();
-  ASSERT_EQ(0, chdir(helper.root_.c_str()));
-
-  Config config;
-  EXPECT_TRUE(config.IsInitialized());
-  EXPECT_STREQ(helper.data_.c_str(), config.datadir().c_str());
-
-  chdir(saved_dir.c_str());
-}
+#endif  // __INCLUDED_SDK_TEST_SDK_HELPER_H__
