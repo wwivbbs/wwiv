c60fef07b98768d49b1439517ac387aa4aacb273
Rushfan <wwiv@users.noreply.github.com>
2014-12-24 15:26:55 +0000

    added tests for ppp config class

--- networkb_test/binkp_config_test.cpp
+++ networkb_test/binkp_config_test.cpp
@@ -12,23 +12,6 @@ using namespace wwiv::strings;
 
 class ParseBinkConfigLineTest : public testing::Test {};
 
-class BinkConfigTest : public testing::Test {
-protected:
-   virtual void SetUp() {
-     const ::testing::TestInfo* const test_info =
-         ::testing::UnitTest::GetInstance()->current_test_info();
-      test_name_ = test_info->name();
-    }
-
-    const string dir() { return files_.TempDir(); }
-    string CreateConfigFile(const string& name, const string& contents) {
-      return files_.CreateTempFile(StrCat(test_name_, "_", name), contents);
-    }
-
-    FileHelper files_;
-    std::string test_name_;
-};
-
 TEST_F(ParseBinkConfigLineTest, NoPort) {
   uint16_t node;
   BinkNodeConfig config;
@@ -61,14 +44,15 @@ TEST_F(ParseBinkConfigLineTest, InvalidLine) {
   ASSERT_FALSE(ParseBinkConfigLine(line, &node, &config));
 }
 
-TEST_F(BinkConfigTest, NodeConfig) {
-  files_.Mkdir("network");
+TEST(BinkConfigTest, NodeConfig) {
+  FileHelper files;
+  files.Mkdir("network");
   const string line("@2 example.com -");
-  files_.CreateTempFile("network/addresses.binkp", line);
-  const string network_dir = files_.DirName("network");
+  files.CreateTempFile("network/addresses.binkp", line);
+  const string network_dir = files.DirName("network");
   BinkConfig config(1, "mybbs", network_dir);
   const BinkNodeConfig* node_config = config.node_config_for(2);
-  ASSERT_NE(nullptr, node_config);
+  ASSERT_TRUE(node_config != nullptr);
   EXPECT_EQ("example.com", node_config->host);
   EXPECT_EQ(24554, node_config->port);
   EXPECT_EQ("-", node_config->password);
