6a757700cfa7ecbe0e37fbf989f9ea37f0ae0320
Rushfan <wwiv@users.noreply.github.com>
2014-12-24 15:26:55 +0000

    added tests for ppp config class

---
+++ networkb_test/ppp_config_test.cpp
@@ -0,0 +1,43 @@
+#include "gtest/gtest.h"
+#include "core/strings.h"
+#include "core_test/file_helper.h"
+#include "networkb/ppp_config.h"
+
+#include <cstdint>
+#include <string>
+
+using std::string;
+using namespace wwiv::net;
+using namespace wwiv::strings;
+
+class ParsePPPConfigLineTest : public testing::Test {};
+
+TEST_F(ParsePPPConfigLineTest, Basic) {
+  uint16_t node;
+  PPPNodeConfig config;
+
+  string line = "@1234 addy@example.com";
+  ASSERT_TRUE(ParseAddressNetLine(line, &node, &config));
+  EXPECT_EQ(1234, node);
+  EXPECT_EQ("addy@example.com", config.email_address);
+}
+
+TEST_F(ParsePPPConfigLineTest, InvalidLine) {
+  uint16_t node;
+  PPPNodeConfig config;
+
+  string line = "*@1234 myhost -";
+  ASSERT_FALSE(ParseAddressNetLine(line, &node, &config));
+}
+
+TEST(PPPConfigTest, NodeConfig) {
+  FileHelper files;
+  files.Mkdir("network");
+  const string line("@2 foo@example.com");
+  files.CreateTempFile("network/ADDRESS.NET", line);
+  const string network_dir = files.DirName("network");
+  PPPConfig config(1, "mybbs", network_dir);
+  const PPPNodeConfig* node_config = config.node_config_for(2);
+  ASSERT_TRUE(node_config != nullptr);
+  EXPECT_EQ("foo@example.com", node_config->email_address);
+}
