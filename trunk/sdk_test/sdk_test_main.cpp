48f07a86429d47caff599acc93c3c81d6e250c2f
rushfan <rushfan@efe8ed7e-8846-41e4-9041-cb610019eb92>
2014-12-27 18:41:44 +0000

    ensure that WWIV_TEST_TEMPDIR is set for SDK tests since it relies on it

--- trunk/sdk_test/sdk_test_main.cpp
+++ trunk/sdk_test/sdk_test_main.cpp
@@ -1,7 +1,17 @@
 #include <cstdio>
+#include <string>
+#include "core/os.h"
 #include "gtest/gtest.h"
+
+using std::string;
 
 int main(int argc, char* argv[]) {
   testing::InitGoogleTest(&argc, argv);
+
+  const string tmpdir = wwiv::os::environment_variable("WWIV_TEST_TEMPDIR");
+  if (tmpdir.empty()) {
+    FAIL() << "WWIV_TEST_TEMPDIR must be set for this test suite.";
+  }
+
   return RUN_ALL_TESTS();
 } 
