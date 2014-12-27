9a954737ebe55589b2c2146fbad35eadd0be92ba
Rushfan <wwiv@users.noreply.github.com>
2014-12-27 03:52:45 +0000

    tests for address line parsing

--- networkb/binkp.h
+++ networkb/binkp.h
@@ -108,6 +108,8 @@ bool ParseFileRequestLine(const std::string& request_line,
 			  time_t* timestamp,
 			  long* offset);
 
+int node_number_from_address_list(const std::string& network_list, const std::string& network_name);
+
 }  // namespace net
 }  // namespace wwiv
 
