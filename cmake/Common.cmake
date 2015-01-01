ba8cb75976c3c25b5115d3daddacaeee7fd53beb
Rushfan <wwiv@users.noreply.github.com>
2015-01-01 01:17:40 +0000

    added support for building under asan (note: this needs GCC 4.9)

---
+++ cmake/Common.cmake
@@ -0,0 +1,11 @@
+
+# Common CMake module for WWIV
+
+cmake_minimum_required(VERSION 2.8 FATAL_ERROR)
+list(APPEND CMAKE_MODULE_PATH ${PROJECT_SOURCE_DIR}/cmake/Modules/sanitizers)
+
+message(STATUS "Loaded Common")
+
+if(CMAKE_SYSTEM_NAME MATCHES "Linux")
+  set(LINUX TRUE)
+endif()
