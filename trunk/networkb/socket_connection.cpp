10dafb1f7953b347931f4306b178c224676db535
rushfan <rushfan@efe8ed7e-8846-41e4-9041-cb610019eb92>
2015-01-14 02:27:44 +0000

    qualify close since we added a close method

--- trunk/networkb/socket_connection.cpp
+++ trunk/networkb/socket_connection.cpp
@@ -25,7 +25,7 @@
 #define NO_ERROR 0
 #define INVALID_SOCKET -1
 #define SOCKET_ERROR -1
-#define closesocket(x) close(x)
+#define closesocket(x) ::close(x)
 #endif  // _WIN32
 
 #include "core/log.h"
