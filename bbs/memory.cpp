11b698bba307b1ef394f03cf085347a9c0fa973e
RushFan <rushfan@wwiv.us>
2015-09-21 20:22:39 -0700

    use calloc vs. malloc + memset

--- bbs/memory.cpp
+++ bbs/memory.cpp
@@ -25,8 +25,7 @@
  * @param lNumBytes Number of bytes to allocate
  */
 void *BbsAllocA(size_t lNumBytes) {
-  void* pBuffer = malloc(lNumBytes + 1);
+  void* pBuffer = calloc(lNumBytes + 1);
   WWIV_ASSERT(pBuffer);
-  memset(pBuffer, 0, lNumBytes);
   return pBuffer;
 }
