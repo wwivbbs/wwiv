475bccff41874246e8400d31e842bc689d4a2edd
RushFan <rushfan@wwiv.us>
2015-11-15 15:28:21 -0800

    fix more file headers

--- networkb/networkb.cpp
+++ networkb/networkb.cpp
@@ -1,18 +1,22 @@
-//
-// NetworkB(inkp) prototype.
-//
-// example usage:
-//
-// Originating:
-// G:\tmp>\Debug\networkb.exe --send --network=rushnet --node=[node number to call]
-//
-// Answering Side:
-// \Debug\networkb.exe --receive --network=rushnet
-//
-// binkp.net:
-// @1 localhost:24554 -
-// @2 localhost:24554 -
-//
+/**************************************************************************/
+/*                                                                        */
+/*                          WWIV Version 5.0x                             */
+/*               Copyright (C)2015, WWIV Software Services                */
+/*                                                                        */
+/*    Licensed  under the  Apache License, Version  2.0 (the "License");  */
+/*    you may not use this  file  except in compliance with the License.  */
+/*    You may obtain a copy of the License at                             */
+/*                                                                        */
+/*                http://www.apache.org/licenses/LICENSE-2.0              */
+/*                                                                        */
+/*    Unless  required  by  applicable  law  or agreed to  in  writing,   */
+/*    software  distributed  under  the  License  is  distributed on an   */
+/*    "AS IS"  BASIS, WITHOUT  WARRANTIES  OR  CONDITIONS OF ANY  KIND,   */
+/*    either  express  or implied.  See  the  License for  the specific   */
+/*    language governing permissions and limitations under the License.   */
+/**************************************************************************/
+
+// WWIV BINKP Network Stack. (networkb.exe)
 
 #include <fcntl.h>
 #include <iostream>
