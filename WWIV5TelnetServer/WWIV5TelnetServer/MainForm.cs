f87fa26038bc824f9f614890a013b4de77d2a40c
TRI0N <tri0n@hotmail.com>
2016-01-13 17:48:45 -0800

    WWIV5TelnetServer v1.1.7
    Corrected Tray Icon and updated Copyright Year in Assembly.

--- WWIV5TelnetServer/WWIV5TelnetServer/MainForm.cs
+++ WWIV5TelnetServer/WWIV5TelnetServer/MainForm.cs
@@ -162,10 +162,10 @@ namespace WWIV5TelnetServer
             Process p = new Process();
             
             // Uncomment For Release Build
-            //p.StartInfo.FileName = "bbs.exe";
+            p.StartInfo.FileName = "bbs.exe";
             
             // Uncomment For Debuging
-            p.StartInfo.FileName = @"C:\wwiv\bbs.exe";
+            //p.StartInfo.FileName = @"C:\wwiv\bbs.exe";
             p.StartInfo.Arguments = "-V";
             p.StartInfo.UseShellExecute = false;
             p.StartInfo.RedirectStandardOutput = true;
