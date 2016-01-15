b2b98827d5562664b8f7f3a963a68dc5ae99eee6
granitepenguin <david@graniteweb.com>
2016-01-15 00:12:56 -0600

    Linux install file updates. Clean up unneeded files and update install script

--- bbs/admin/unix/bin/processmail.sh
+++ bbs/admin/unix/bin/processmail.sh
@@ -4,12 +4,17 @@
 #
 # script to manage all the steps for processing WWIVnet packets
 #
+# This only covers 
 
 source REPLACE-WWIVBASE/.wwivrc
 
 date
 
 cd ${WWIV_DIR}
-${WWIV_DIR}/bin/inbound.sh
-${WWIV_DIR}/bin/outbound.sh
+
+# I'd like to eventually be able to loop over the list of 
+# nets from init data
+
+# Calls out to @1 of WWIVnet
+./networkb --send --network=wwivnet --node=1 --bbsdir=${WWIV_DIR}
 
