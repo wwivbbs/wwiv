0d0d59a95c6c89a58aa598b852af52b7b36c2d69
TRI0N <tri0n@hotmail.com>
2016-01-26 18:24:23 -0800

    WWIV5TelnetServer v1.2.0.0 SSH Enabled
    Added SSH Server for Listening on Port 22.

---
+++ WWIV5TelnetServer/WWIV5TelnetServer/LogForm.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace WWIV5TelnetServer
+{
+    partial class LogForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LogForm));
+            this.tabNetwork = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.tabNetwork.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabNetwork
+            // 
+            this.tabNetwork.Controls.Add(this.tabPage1);
+            this.tabNetwork.Controls.Add(this.tabPage2);
+            this.tabNetwork.Controls.Add(this.tabPage3);
+            this.tabNetwork.Location = new System.Drawing.Point(2, 1);
+            this.tabNetwork.Name = "tabNetwork";
+            this.tabNetwork.SelectedIndex = 0;
+            this.tabNetwork.Size = new System.Drawing.Size(660, 335);
+            this.tabNetwork.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.listBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(652, 309);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Network Log";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Lucida Console", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(0, 3);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(649, 304);
+            this.listBox1.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.listBox2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(652, 309);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Networkb Log";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.listBox3);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(652, 309);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "WWIVSync Log";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // listBox2
+            // 
+            this.listBox2.Font = new System.Drawing.Font("Lucida Console", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 12;
+            this.listBox2.Location = new System.Drawing.Point(0, 3);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(649, 304);
+            this.listBox2.TabIndex = 1;
+            // 
+            // listBox3
+            // 
+            this.listBox3.Font = new System.Drawing.Font("Lucida Console", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 12;
+            this.listBox3.Location = new System.Drawing.Point(0, 3);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(649, 304);
+            this.listBox3.TabIndex = 1;
+            // 
+            // LogForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(663, 337);
+            this.Controls.Add(this.tabNetwork);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "LogForm";
+            this.Text = "WWIV 5 Logs";
+            this.Load += new System.EventHandler(this.LogForm_Load);
+            this.tabNetwork.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabNetwork;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+    }
+}
---
+++ WWIV5TelnetServer/WWIV5TelnetServer/LogForm.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WWIV5TelnetServer
+{
+    public partial class LogForm : Form
+    {
+        public LogForm()
+        {
+            InitializeComponent();
+        }
+
+        private void LogForm_Load(object sender, EventArgs e)
+        {
+            // Network Log
+            // Pass The File Path and File Name To The StreamReader Constructor
+            StreamReader sr1 = new StreamReader(Properties.Settings.Default.homeDirectory + @"\network.log");
+            string line1 = string.Empty;
+            try
+            {
+                // Read The First Line Of Text
+                line1 = sr1.ReadLine();
+                string errorText = "ERROR:";
+
+                // Continue To Read Till End Of File
+                while (line1 != null)
+                {
+                    // Catch ERROR: And Color Red Else Green
+                    if (!line1.Contains(errorText))
+                    {
+                        listBox1.ForeColor = Color.Green;
+                    }
+                    else
+                    {
+                        listBox1.ForeColor = Color.DarkRed;
+                    }
+
+                    // Create List Item
+                    this.listBox1.Items.Add(line1);
+
+                    // Read The Next Line
+                    line1 = sr1.ReadLine();
+                }
+
+                //close the file
+                sr1.Close();
+            }
+            catch (Exception ex1)
+            {
+                MessageBox.Show(ex1.Message.ToString());
+            }
+            finally
+            {
+                //close the file
+                sr1.Close();
+            }
+
+            // Networkb Log
+            string errorText2 = " ERROR: ";
+            //const int max = 500;
+            // From Bottom To Top
+            var lines2 = File.ReadAllLines(Properties.Settings.Default.homeDirectory + @"\networkb.log").Reverse();
+
+            foreach (string line2 in lines2)
+            {
+                // Default Color Green
+                listBox2.ForeColor = Color.Green;
+
+                // Catch ERROR: And Color Red Else Green
+                if (line2.Contains(errorText2))
+                {
+                    listBox2.ForeColor = Color.DarkRed;
+                }
+                this.listBox2.Items.Add(line2);
+            }
+
+            /*FileStream fs = new FileStream(Properties.Settings.Default.homeDirectory + @"\networkb.log", FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            fs = fs.Seek(-20000, SeekOrigin.End);
+            StreamReader sr = new StreamReader(fs);
+            string x = sr.ReadToEnd();
+            sr.Close();
+
+            // Pass The File Path and File Name To The StreamReader Constructor
+            StreamReader sr2 = new StreamReader(Properties.Settings.Default.homeDirectory + @"\networkb.log");
+            string line2 = string.Empty;
+            try
+            {
+                // Read The First Line Of Text
+                line2 = sr2.ReadLine();
+                string errorText = "ERROR:";
+
+                // Continue To Read Till End Of File
+                while (line2 != null)
+                {
+                    // Catch ERROR: And Color Red Else Green
+                    if (!line2.Contains(errorText))
+                    {
+                        listBox2.ForeColor = Color.Green;
+                    }
+                    else
+                    {
+                        listBox2.ForeColor = Color.DarkRed;
+                    }
+
+                    // Create List Item
+                    this.listBox2.Items.Add(line2);
+
+                    // Read The Next Line
+                    line2 = sr2.ReadLine();
+                }
+
+                //close the file
+                sr2.Close();
+            }
+            catch (Exception ex2)
+            {
+                MessageBox.Show(ex2.Message.ToString());
+            }
+            finally
+            {
+                //close the file
+                sr2.Close();
+            }*/
+
+            // WWIVSync Log
+            // Pass The File Path and File Name To The StreamReader Constructor
+            StreamReader sr3 = new StreamReader(Properties.Settings.Default.homeDirectory + @"\wwivsync.log");
+            string line3 = string.Empty;
+            try
+            {
+                // Read The First Line Of Text
+                line3 = sr3.ReadLine();
+                string errorText = "ERROR:";
+
+                // Continue To Read Till End Of File
+                while (line3 != null)
+                {
+                    // Catch ERROR: And Color Red Else Green
+                    if (!line3.Contains(errorText))
+                    {
+                        listBox3.ForeColor = Color.Green;
+                    }
+                    else
+                    {
+                        listBox3.ForeColor = Color.DarkRed;
+                    }
+
+                    // Create List Item
+                    this.listBox3.Items.Add(line3);
+
+                    // Read The Next Line
+                    line3 = sr3.ReadLine();
+                }
+
+                //close the file
+                sr3.Close();
+            }
+            catch (Exception ex3)
+            {
+                MessageBox.Show(ex3.Message.ToString());
+            }
+            finally
+            {
+                //close the file
+                sr3.Close();
+            }
+        }
+    }
+}
