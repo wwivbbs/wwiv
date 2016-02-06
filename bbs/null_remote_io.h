11149a15862489c28971cb2dd4bb15e28525152a
RushFan <rushfan@wwivbbs.org>
2016-02-05 23:48:15 -0800

    add a null remote IO that we use when launching the bbs in local mode.

--- bbs/null_remote_io.h
+++ bbs/null_remote_io.h
@@ -17,70 +17,35 @@
 /*                                                                        */
 /**************************************************************************/
 
-#if !defined (__INCLUDED_BBS_REMOTE_IO_H__)
-#define __INCLUDED_BBS_REMOTE_IO_H__
+#if !defined (__INCLUDED_BBS_NULL_REMOTE_IO_H__)
+#define __INCLUDED_BBS_NULL_REMOTE_IO_H__
 
 #include <string>
 
-enum class CommunicationType {
-  NONE,
-  TELNET,
-  SSH,
-  STDIO,
-  UNIX
-};
+#include "bbs/remote_io.h"
 
-/** Information about the remote session. */
-class RemoteInfo {
-public:
-  void clear() {
-    cid_name.clear();
-    address.clear();
-    username.clear();
-    password.clear();
-  }
-
-  std::string cid_name;
-  std::string address;
-  std::string username;
-  std::string password;
-};
+/** Null remote session. */
 /**
  * Base Communication Class.
  */
-class RemoteIO {
+class NullRemoteIO : public RemoteIO {
  public:
-  RemoteIO() : binary_mode_(false) {}
-  virtual ~RemoteIO() {}
+  NullRemoteIO() : RemoteIO() {}
+  virtual ~NullRemoteIO() {}
 
-  virtual bool open() = 0;
-  virtual void close(bool temporary) = 0;
-  virtual unsigned char getW() = 0;
-  virtual bool dtr(bool raise) = 0;
-  virtual void purgeIn() = 0;
-  virtual unsigned int put(unsigned char ch) = 0;
-  virtual unsigned int read(char *buffer, unsigned int count) = 0;
-  virtual unsigned int write(const char *buffer, unsigned int count, bool bNoTranslation = false) = 0;
-  virtual bool carrier() = 0;
-  virtual bool incoming() = 0;
+  // Nothing is always able to be open.
+  bool open() override { return true; }
+  void close(bool temporary) override {}
+  unsigned char getW() override { return 0; }
+  bool dtr(bool raise) override { return true; }
+  void purgeIn() override {}
+  unsigned int put(unsigned char ch) override { return 0; }
+  unsigned int read(char *buffer, unsigned int count) override { return 0; }
+  unsigned int write(const char *buffer, unsigned int count, bool bNoTranslation = false) override { return 0; }
+  bool carrier() override { return false; }
+  bool incoming() override { return false; }
 
-  virtual unsigned int GetHandle() const = 0;
-  virtual unsigned int GetDoorHandle() const { return GetHandle(); }
-
-  void set_binary_mode(bool b) { binary_mode_ = b; }
-  bool binary_mode() const { return binary_mode_; }
-
-  RemoteInfo& remote_info() { return remote_info_; }
-
-protected:
-  bool binary_mode_;
-
-  static const std::string GetLastErrorText();
-
-private:
-  // used by the GetLastErrorText() method
-  static std::string error_text_;
-  RemoteInfo remote_info_;
+  unsigned int GetHandle() const override { return 0; }
 };
 
 #endif  // #if !defined (__INCLUDED_BBS_REMOTE_IO_H__)
