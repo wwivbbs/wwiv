c8766afc43d24be3611acba2995919d96ceb466b
RushFan <rushfan@wwivbbs.org>
2016-05-21 15:59:22 -0700

    update the test command to work with solution level output

--- builds/jenkins/wwiv/test.cmd
+++ builds/jenkins/wwiv/test.cmd
@@ -6,22 +6,22 @@ echo "WWIV_TEST_TEMPDIR: %WWIV_TEST_TEMPDIR%"
 cd %WORKSPACE%
 msbuild WWIV.sln /t:Build /p:Configuration=Debug || exit /b
 
-cd %WORKSPACE%\core_test\Debug
+cd %WORKSPACE%\Debug
 del result.xml
 dir
 core_test.exe --gtest_output=xml:result-core.xml
 
-cd %WORKSPACE%\bbs_test\Debug
+cd %WORKSPACE%\Debug
 del result.xml
 dir
 bbs_test.exe --gtest_output=xml:result-bbs.xml
 
-cd %WORKSPACE%\sdk_test\Debug
+cd %WORKSPACE%\Debug
 del result.xml
 dir
 sdk_test.exe --gtest_output=xml:result-sdk.xml
 
-cd %WORKSPACE%\networkb_test\Debug
+cd %WORKSPACE%\Debug
 del result.xml
 dir
 networkb_test.exe --gtest_output=xml:result-networkb.xml
