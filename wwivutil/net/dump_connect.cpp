a5320767ac7e541d73b5fcad38ff794b2ea71f34
RushFan <rushfan@wwivbbs.org>
2016-06-25 07:47:25 -0700

    add missing dump_connect files

---
+++ wwivutil/net/dump_connect.cpp
@@ -0,0 +1,73 @@
+/**************************************************************************/
+/*                                                                        */
+/*                          WWIV Version 5.x                              */
+/*             Copyright (C)2015-2016 WWIV Software Services              */
+/*                                                                        */
+/*    Licensed  under the  Apache License, Version  2.0 (the "License");  */
+/*    you may not use this  file  except in compliance with the License.  */
+/*    You may obtain a copy of the License at                             */
+/*                                                                        */
+/*                http://www.apache.org/licenses/LICENSE-2.0              */
+/*                                                                        */
+/*    Unless  required  by  applicable  law  or agreed to  in  writing,   */
+/*    software  distributed  under  the  License  is  distributed on an   */
+/*    "AS IS"  BASIS, WITHOUT  WARRANTIES  OR  CONDITIONS OF ANY  KIND,   */
+/*    either  express  or implied.  See  the  License for  the specific   */
+/*    language governing permissions and limitations under the License.   */
+/**************************************************************************/
+#include "wwivutil/net/dump_connect.h"
+
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+#include "core/log.h"
+#include "core/strings.h"
+#include "sdk/config.h"
+#include "sdk/connect.h"
+#include "sdk/config.h"
+#include "sdk/networks.h"
+
+using std::cout;
+using std::endl;
+using std::map;
+using std::string;
+using namespace wwiv::sdk;
+using namespace wwiv::strings;
+
+namespace wwiv {
+namespace wwivutil {
+
+std::string DumpConnectCommand::GetUsage() const {
+  std::ostringstream ss;
+  ss << "Usage:   dump_connect" << endl;
+  ss << "Example: dump_connect" << endl;
+  return ss.str();
+}
+
+int DumpConnectCommand::Execute() {
+  Networks networks(*config()->config());
+  if (!networks.IsInitialized()) {
+    LOG << "Unable to load networks.";
+    return 1;
+  }
+
+  map<const string, Connect> connects;
+  for (const auto net : networks.networks()) {
+    string lower_case_network_name(net.name);
+    StringLowerCase(&lower_case_network_name);
+    connects.emplace(lower_case_network_name, Connect(net.dir));
+  }
+
+  for (const auto& c : connects) {
+    cout << "CONNECT.NET information: : " << c.first << endl;
+    cout << "===========================================================" << endl;
+    cout << c.second.ToString() << endl;
+  }
+
+  return 0;
+}
+
+
+}
+}
---
+++ wwivutil/net/dump_connect.h
@@ -0,0 +1,44 @@
+/**************************************************************************/
+/*                                                                        */
+/*                              WWIV Version 5.x                          */
+/*             Copyright (C)1998-2016, WWIV Software Services             */
+/*                                                                        */
+/*    Licensed  under the  Apache License, Version  2.0 (the "License");  */
+/*    you may not use this  file  except in compliance with the License.  */
+/*    You may obtain a copy of the License at                             */
+/*                                                                        */
+/*                http://www.apache.org/licenses/LICENSE-2.0              */
+/*                                                                        */
+/*    Unless  required  by  applicable  law  or agreed to  in  writing,   */
+/*    software  distributed  under  the  License  is  distributed on an   */
+/*    "AS IS"  BASIS, WITHOUT  WARRANTIES  OR  CONDITIONS OF ANY  KIND,   */
+/*    either  express  or implied.  See  the  License for  the specific   */
+/*    language governing permissions and limitations under the License.   */
+/**************************************************************************/
+#ifndef __INCLUDED_WWIVUTIL_DUMP_CONNECT_H__
+#define __INCLUDED_WWIVUTIL_DUMP_CONNECT_H__
+
+#include <map>
+#include <string>
+
+#include "core/command_line.h"
+#include "sdk/connect.h"
+#include "wwivutil/command.h"
+
+namespace wwiv {
+namespace wwivutil {
+
+class DumpConnectCommand final: public UtilCommand {
+public:
+  DumpConnectCommand()
+    : UtilCommand("dump_connect", "Dumps parsed representation of CONNECT.NET") {}
+  int Execute() override final;
+  std::string GetUsage() const override final;
+  bool AddSubCommands() override final { return true; }
+};
+
+
+}  // namespace wwivutil
+}  // namespace wwiv
+
+#endif  // __INCLUDED_WWIVUTIL_DUMP_CONNECT_H__
