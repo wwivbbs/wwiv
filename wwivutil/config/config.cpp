0d89db56772ef525a9400d4025cbed79a4408349
RushFan <rushfan@wwivbbs.org>
2016-10-08 13:19:10 -0700

    add config

---
+++ wwivutil/config/config.cpp
@@ -0,0 +1,73 @@
+/**************************************************************************/
+/*                                                                        */
+/*                          WWIV Version 5.x                              */
+/*             Copyright (C)2015-2016 WWIV Software Services              */
+/*                                                                        */
+/*    Licensed  under the  Apache License, Version  2.0 (the "License");  */
+/*    you may not use this  file  except in compliance with the License.  */
+/*    You may obtain a copy of the License at                             */
+/*                                                                        */
+/*                http://www.apache.org/licenses/LICENSE-2.0              */
+/*                                                                        */
+/*    Unless  required  by  applicable  law  or agreed to  in  writing,   */
+/*    software  distributed  under  the  License  is  distributed on an   */
+/*    "AS IS"  BASIS, WITHOUT  WARRANTIES  OR  CONDITIONS OF ANY  KIND,   */
+/*    either  express  or implied.  See  the  License for  the specific   */
+/*    language governing permissions and limitations under the License.   */
+/**************************************************************************/
+#include "wwivutil/config/config.h"
+
+#include <cstdio>
+#include <iomanip>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
+#include "core/command_line.h"
+#include "core/file.h"
+#include "core/strings.h"
+#include "sdk/config.h"
+#include "sdk/net.h"
+#include "sdk/networks.h"
+
+#include "wwivutil/net/dump_bbsdata.h"
+#include "wwivutil/net/dump_callout.h"
+#include "wwivutil/net/dump_connect.h"
+#include "wwivutil/net/dump_contact.h"
+#include "wwivutil/net/dump_packet.h"
+
+using std::endl;
+using std::make_unique;
+using std::setw;
+using std::string;
+using std::unique_ptr;
+using std::vector;
+using wwiv::core::BooleanCommandLineArgument;
+using namespace wwiv::sdk;
+
+namespace wwiv {
+namespace wwivutil {
+
+class ConfigVersionCommand : public UtilCommand {
+public:
+  ConfigVersionCommand(): UtilCommand("version", "Sets or Gets the config version") {}
+  std::string GetUsage() const override final {
+    return "";
+  }
+  int Execute() override final {
+    return 0;
+  }
+  bool AddSubCommands() override final {
+    return true;
+  }
+
+};
+
+bool ConfigCommand::AddSubCommands() {
+  add(make_unique<ConfigVersionCommand>());
+  return true;
+}
+
+
+}  // namespace wwivutil
+}  // namespace wwiv
