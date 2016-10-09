0a1f7d416be22e9847e388eb93d8db46dbab1d33
Rushfan <rushfan@wwivbbs.org>
2016-10-08 19:09:45 -0700

    systemd setup, redid install script.

--- bbs/admin/unix/bin/processmail.sh
+++ bbs/admin/unix/bin/processmail.sh
@@ -7,7 +7,7 @@
 # This only covers a basic connetion to the main WWIVnet node.
 # You can add multiple network calls if you add other nets.
 
-source REPLACE-WWIVBASE/.wwivrc
+source /etc/default/wwivd
 
 date
 
@@ -17,5 +17,5 @@ cd ${WWIV_DIR}
 # nets from init data.  Currently it has to be done manually
 
 # Calls out to @1 of WWIVnet (assuming your first network is 0)
-./network --net=0 --node=1 --bbsdir=${WWIV_DIR}
+./network /n1
 
--- bbs/admin/unix/install.sh
+++ bbs/admin/unix/install.sh
@@ -6,39 +6,89 @@
 # in order to facilitate the setup.  In particular, we assume we are running
 # from the install wwivbase location.
 
-WWIV_DIR=`pwd`
+WWIV_DIR=$(pwd)
+WWIV_USER=$(whoami)
 LOGFILE=install_`date "+%Y-%m-%d_%H-%M-%S"`.log
 
-echo
-echo "Starting the install process"
-echo "Starting the install process" > ${WWIV_DIR}/$LOGFILE 2>&1
-echo
+say() {
+  echo $1
+  echo $1 > ${WWIV_DIR}/${LOGFILE}
+}
+
+confirm () {
+    # call with a prompt string or use a default
+    read -r -p "${1:-Are you sure? [y/N]} " response
+    case $response in
+        [yY][eE][sS]|[yY]) 
+            true
+            ;;
+        *)
+            false
+            ;;
+    esac
+}
+
+cat <<EOF
+
+ _       ___       __ __ _    __   ______  ___   
+| |     / / |     / /  _/ |  / /  / ____/ |__ \ 
+| | /| / /| | /| / // / | | / /  /___ \   __/ / 
+| |/ |/ / | |/ |/ // /  | |/ /  ____/ /_ / __/  
+|__/|__/  |__/|__/___/  |___/  /_____/(_)____/  
+                                               
+    ____           __        ____      __  _           
+   /  _/___  _____/ /_____ _/ / /___ _/ /_(_)___  ____ 
+   / // __ \/ ___/ __/ __  / / / __  / __/ / __ \/ __ \\ 
+ _/ // / / (__  ) /_/ /_/ / / / /_/ / /_/ / /_/ / / / / 
+/___/_/ /_/____/\__/\__,_/_/_/\__,_/\__/_/\____/_/ /_/  
+ 
+
+
+EOF
+
+say "Starting the install process."
+say ""
+
+say "You are running as: ${WWIV_USER}"
+say "Please make sure you are running as the user you will use for WWIV."
+echo 
+echo 
+confirm "Are you running as the correct WWIV user (${WWIV_USER})? " || exit 1
 
 # Make sure init exists
-if [ ! -f ${WWIV_DIR}/init ]
+if [ ! -f "${WWIV_DIR}/init" ]
 then
-    echo "init is missing.  Please compile your BBS files and copy them"
-    echo "init is missing.  Please compile your BBS files and copy them" >> ${WWIV_DIR}/$LOGFILE 2>&1
-    echo "to ${WWIV_DIR} and run install.sh again."
-    echo "to ${WWIV_DIR} and run install.sh again." >> ${WWIV_DIR}/$LOGFILE 2>&1
+    say "init is missing. Please compile your BBS files and copy them"
+    say "to ${WWIV_DIR} and run install.sh again."
     exit 1
 fi
 
 # configure scripts and helper binaries.
-echo "Configuring system scripts"
-echo "Configuring system scripts" >> ${WWIV_DIR}/$LOGFILE 2>&1
+echo 
+echo 
+say ""
+say "Configuring scripts to use the following settings:"
+
+say "WWIV_DIR:  ${WWIV_DIR}"
+say "WWIV_USER: ${WWIV_USER}"
 echo
-echo "Setting scripts to use your install location ${WWIV_DIR}"
-echo "Setting scripts to use your install location ${WWIV_DIR}" >> ${WWIV_DIR}/$LOGFILE 2>&1
-for i in bin/processmail.sh bin/daily-cleanup.sh .wwivrc in.nodemgr wwiv-service
+echo
+
+for i in systemd/*.template
 do
-    sed -i "s@REPLACE-WWIVBASE@${WWIV_DIR}@" $i
+	out=$(echo $i | sed -e 's/\.[^.]*$//')
+	echo "Processing: $i > ${out}"
+    WWIV_USER=${WWIV_USER} WWIV_DIR=${WWIV_DIR} envsubst < $i > ${out}
 done
 
 echo
-echo "Your BBS basic data setup is complete."
-echo "running ./init now to finalize the BBS.  "
-sleep 5
+echo
+echo "Your BBS basic systemd service configuration "
+echo "and helper scripts are complete."
+echo
+echo
+
+confirm "Press enter to run ./init to finalize setting up the BBS: "
 
 ./init
 
@@ -46,3 +96,7 @@ echo
 echo "init complete.  Please run ./bbs and hit the spacebar to login"
 echo "and set up a new user to be the sysop (#1) account"
 echo
+echo "To create a systemd service, please copy the following files (as root):"
+echo
+echo "sudo cp $(pwd)/systemd/wwivd.default /etc/default/wwivd"
+echo "sudo cp $(pwd)/systemd/wwivd.service /etc/systemd/system/wwivd.service"
