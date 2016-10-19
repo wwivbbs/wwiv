5b4bb800d06c521d7d7b76507568a75a7bb98f99
RushFan <rushfan@wwivbbs.org>
2016-10-18 21:25:17 -0700

    prep for win2016 build machine
    - new build machine will use the ms command line build tools
      (not a full msvc install).  That doesn't have texttransform
      so let's use sed on windows (from git) to stamp version.cpp
    - The command line build tools have windows sdk 10.0.10240 not
      10.0.10586, so we'll use that (it doesn't really matter, we could
      even use the 8.1 SDK problably) but the 10 SDK fixes *LOTS* of compiler
      warnings in the windows header files.
    -

--- builds/jenkins/wwiv/rushfan-build.cmd
+++ builds/jenkins/wwiv/rushfan-build.cmd
@@ -1,6 +1,7 @@
 SET TEXT_TRANSFORM="C:\Program Files\Common Files\microsoft shared\TextTemplating\14.0\TextTransform.exe"
 SET WORKSPACE=W:\wwiv
 SET BUILD_NUMBER=2112
+SET SED="C:\Program Files\Git\usr\bin\sed.exe"
 pushd %WORKSPACE%
 copy %WORKSPACE%\core\version.cpp %WORKSPACE%\core\version.cpp.saved
 call %WORKSPACE%\builds\jenkins\wwiv\build.cmd || echo "Build FAILED!"
