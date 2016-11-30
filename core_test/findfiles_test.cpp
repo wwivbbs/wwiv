0e3f50df82a8cf3e45320ed19a4f4b83d0617450
RushFan <rushfan@wwivbbs.org>
2016-11-29 19:54:03 -0800

    add test for wfndfiles

---
+++ core_test/findfiles_test.cpp
@@ -0,0 +1,77 @@
+/**************************************************************************/
+/*                                                                        */
+/*                              WWIV Version 5.x                          */
+/*               Copyright (C)2014-2016 WWIV Software Services            */
+/*                                                                        */
+/*    Licensed  under the  Apache License, Version  2.0 (the "License");  */
+/*    you may not use this  file  except in compliance with the License.  */
+/*    You may obtain a copy of the License at                             */
+/*                                                                        */
+/*                http://www.apache.org/licenses/LICENSE-2.0              */
+/*                                                                        */
+/*    Unless  required  by  applicable  law  or agreed to  in  writing,   */
+/*    software  distributed  under  the  License  is  distributed on an   */
+/*    "AS IS"  BASIS, WITHOUT  WARRANTIES  OR  CONDITIONS OF ANY  KIND,   */
+/*    either  express  or implied.  See  the  License for  the specific   */
+/*    language governing permissions and limitations under the License.   */
+/*                                                                        */
+/**************************************************************************/
+#include "file_helper.h"
+#include "gtest/gtest.h"
+#include "core/file.h"
+#include "core/wfndfile.h"
+#include "core/strings.h"
+
+#include <iostream>
+#include <string>
+
+using std::string;
+using namespace wwiv::core;
+using namespace wwiv::strings;
+
+TEST(FindFiles, Suffix) {
+  FileHelper helper;
+  helper.CreateTempFile("msg00000.001", "");
+  WFindFile fnd;
+  ASSERT_TRUE(fnd.open(FilePath(helper.TempDir(), "msg*"), 0));
+  EXPECT_STREQ("msg00000.001", fnd.GetFileName());
+  EXPECT_FALSE(fnd.next());
+}
+
+TEST(FindFiles, Prefix) {
+  FileHelper helper;
+  helper.CreateTempFile("msg00000.001", "");
+  WFindFile fnd;
+  ASSERT_TRUE(fnd.open(FilePath(helper.TempDir(), "*001"), 0));
+  EXPECT_STREQ("msg00000.001", fnd.GetFileName());
+  EXPECT_FALSE(fnd.next());
+}
+
+TEST(FindFiles, SingleCharSuffix) {
+  FileHelper helper;
+  helper.CreateTempFile("msg00000.001", "");
+  WFindFile fnd;
+  ASSERT_TRUE(fnd.open(FilePath(helper.TempDir(), "msg00000.00?"), 0));
+  EXPECT_STREQ("msg00000.001", fnd.GetFileName());
+  EXPECT_FALSE(fnd.next());
+}
+
+TEST(FindFiles, SingleCharPrefix) {
+  FileHelper helper;
+  helper.CreateTempFile("msg00000.001", "");
+  WFindFile fnd;
+  ASSERT_TRUE(fnd.open(FilePath(helper.TempDir(), "?sg00000.001"), 0));
+  EXPECT_STREQ("msg00000.001", fnd.GetFileName());
+  EXPECT_FALSE(fnd.next());
+}
+
+TEST(FindFiles, SingleCharExtensionPrefix) {
+  FileHelper helper;
+  helper.CreateTempFile("msg00000.001", "");
+  WFindFile fnd;
+  ASSERT_TRUE(fnd.open(FilePath(helper.TempDir(), "msg00000.?01"), 0));
+  EXPECT_STREQ("msg00000.001", fnd.GetFileName());
+  EXPECT_FALSE(fnd.next());
+}
+
+
