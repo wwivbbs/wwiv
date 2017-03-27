456f2c125636c394a9ac9486973ecec6ce74ab78
RushFan <rushfan@wwivbbs.org>
2017-03-26 22:18:02 -0700

    add %I to stuff_in

--- bbs/stuffin.cpp
+++ bbs/stuffin.cpp
@@ -43,24 +43,24 @@ const unsigned int GetTimeLeft();
 // Replacable parameters:
 // ~~~~~~~~~~~~~~~~~~~~~~
 //
-// Param     Description                       Example
-// ---------------------------------------------------------------------
-//  %%       A single '%'                      "%"
+//  Param    Description                       Example
+//  ----------------------------------------------------------------------------
+//  %%       A single '%'                          "%"
 //  %1-%5    Specified passed-in parameter
-//  %A       callinfo full pathname            "c:\wwiv\temp\callinfo.bbs"
-//  %C       chain.txt full pathname           "c:\wwiv\temp\chain.txt"
-//  %D       doorinfo full pathname            "c:\wwiv\temp\dorinfo1.def"
-//  %E       door32.sys full pathname          "C:\wwiv\temp\door32.sys"
-//  %H       Socket Handle                     "1234"
-//  %K       gfiles comment file for archives  "c:\wwiv\gfiles\comment.txt"
-//  %M       Modem baud rate                   "38400"
-//  %N       Instance number                   "1"
-//  %O       pcboard full pathname             "c:\wwiv\temp\pcboard.sys"
-//  %P       Com port number                   "1"
-//  %R       door full pathname                "c:\wwiv\temp\door.sys"
-//  %S       Com port baud rate                "38400"
-//  %T       Time remaining (min)              "30"
-//
+//  %A       callinfo full pathname                "c:\wwiv\temp\callinfo.bbs"
+//  %C       chain.txt full pathname               "c:\wwiv\temp\chain.txt"
+//  %D       doorinfo full pathname                "c:\wwiv\temp\dorinfo1.def"
+//  %E       door32.sys full pathname              "C:\wwiv\temp\door32.sys"
+//  %H       Socket Handle                         "1234"
+//  %I       Full Path to TEMP instance directory  "C:\wwiv\temp"
+//  %K       gfiles comment file for archives      "c:\wwiv\gfiles\comment.txt"
+//  %M       Modem BPS rate                         "38400"
+//  %N       Instance number                       "1"
+//  %O       pcboard full pathname                 "c:\wwiv\temp\pcboard.sys"
+//  %P       Com port number                       "1"
+//  %R       door full pathname                    "c:\wwiv\temp\door.sys"
+//  %S       Com port BPS rate                     "38400"
+//  %T       Time remaining (min)                  "30"
 
 /**
  * @todo Document this
@@ -111,6 +111,7 @@ const string stuff_in(const string& commandline, const string& arg1,
       case 'H':
         os << a()->remoteIO()->GetDoorHandle();
         break;
+      case 'I': os << a()->temp_directory(); break;
       case 'K':
         os << a()->config()->gfilesdir() << COMMENT_TXT;
         break;
