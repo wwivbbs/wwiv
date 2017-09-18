2da35cda394b07b294780ce71ace640e1c8f11a4
RushFan <rushfan@wwivbbs.org>
2017-09-17 21:47:00 -0700

    added missing cmake file

---
+++ cmake/FindWWIVCurses.cmake
@@ -0,0 +1,16 @@
+#
+# FindWWIVCurses.
+#
+# On Windows, WWIV uses PDCurses. On all other platforms, use ncurses.
+
+if(UNIX)
+  message("FindWWIVCurses: Using NCurses")
+  set(CURSES_NEED_NCURSES TRUE)
+  find_package(Curses REQUIRED)
+elseif(WIN32)
+  message("FindWWIVCurses: Using PDCurses")
+  include_directories(${PDCURSES_INCLUDE_DIRS})
+  message("FindWWIVCurses: PDCURSES_INCLUDE_DIRS: ${PDCURSES_INCLUDE_DIRS}")
+  message("FindWWIVCurses: CURSES_LIBRARIES: ${CURSES_LIBRARIES}")
+endif()
+message("FindWWIVCurses: Curses Library: ${CURSES_LIBRARIES}") 
