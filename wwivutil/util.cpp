245876acef764033c56f7b45e5203e68a5bbcf91
rushfan <rushfan@wwivbbs.org>
2018-06-18 07:09:13 -0700

    added util.{cpp,h}, missed last time

---
+++ wwivutil/util.cpp
@@ -0,0 +1,70 @@
+/**************************************************************************/
+/*                                                                        */
+/*                              WWIV Version 5.x                          */
+/*                  Copyright (C)2018, WWIV Software Services             */
+/*                                                                        */
+/*    Licensed  under the  Apache License, Version  2.0 (the "License");  */
+/*    you may not use this  file  except in compliance with the License.  */
+/*    You may obtain a copy of the License at                             */
+/*                                                                        */
+/*                http://www.apache.org/licenses/LICENSE-2.0              */
+/*                                                                        */
+/*    Unless  required  by  applicable  law  or agreed to  in  writing,   */
+/*    software  distributed  under  the  License  is  distributed on an   */
+/*    "AS IS"  BASIS, WITHOUT  WARRANTIES  OR  CONDITIONS OF ANY  KIND,   */
+/*    either  express  or implied.  See  the  License for  the specific   */
+/*    language governing permissions and limitations under the License.   */
+/*                                                                        */
+/**************************************************************************/
+#include "wwivutil/util.h"
+
+namespace wwiv {
+namespace wwivutil {
+
+void dump_char(std::ostream& out, char ch) { 
+  
+  switch (ch) { 
+  case 1:
+  case 2:
+  case 3:
+  case 4:
+  case 5:
+  case 6:
+  case 7:
+  case 8:
+  case 9:
+  case 11:
+  case 12:
+  case 14:
+  case 15:
+  case 16:
+  case 17:
+  case 18:
+  case 19:
+  case 20:
+  case 21:
+  case 22:
+  case 23:
+  case 24:
+  case 25:
+  case 26:
+    out << "[^" << static_cast<char>('@' + ch) << "]";
+    break;
+  case 27:
+    out << "[ESC]";
+    break;
+  case 28:
+  case 29:
+  case 30:
+  case 31:
+    out << "[#" << ch << "]";
+    break;
+  default:
+      out << ch;
+    break;
+  }
+}
+
+
+} // namespace wwivutil 
+} // namespace wwiv
