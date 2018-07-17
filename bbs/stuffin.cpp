b2ef9001f298320c90313954655145b621448e43
rushfan <rushfan@wwivbbs.org>
2018-07-17 09:57:02 -0700

    mention docs of parameters + cleanup

--- bbs/stuffin.cpp
+++ bbs/stuffin.cpp
@@ -23,14 +23,14 @@
 
 #include "core/strings.h"
 
-#include "bbs/dropfile.h"
-#include "bbs/remote_io.h"
-#include "bbs/wconstants.h"
 #include "bbs/application.h"
 #include "bbs/bbs.h"
 #include "bbs/bbsutl.h"
+#include "bbs/dropfile.h"
+#include "bbs/remote_io.h"
 #include "bbs/utility.h"
 #include "bbs/vars.h"
+#include "bbs/wconstants.h"
 #include "sdk/filenames.h"
 
 using std::string;
@@ -39,37 +39,39 @@ using namespace wwiv::strings;
 
 const unsigned int GetTimeLeft();
 
-
-// Replacable parameters:
-// ~~~~~~~~~~~~~~~~~~~~~~
-//
-//  Param    Description                       Example
-//  ----------------------------------------------------------------------------
-//  %%       A single '%'                          "%"
-//  %1-%5    Specified passed-in parameter
-//  %A       callinfo full pathname                "c:\wwiv\temp\callinfo.bbs"
-//  %C       chain.txt full pathname               "c:\wwiv\temp\chain.txt"
-//  %D       doorinfo full pathname                "c:\wwiv\temp\dorinfo1.def"
-//  %E       door32.sys full pathname              "C:\wwiv\temp\door32.sys"
-//  %H       Socket Handle                         "1234"
-//  %I       Full Path to TEMP instance directory  "C:\wwiv\temp"
-//  %K       gfiles comment file for archives      "c:\wwiv\gfiles\comment.txt"
-//  %M       Modem BPS rate                         "38400"
-//  %N       Instance number                       "1"
-//  %O       pcboard full pathname                 "c:\wwiv\temp\pcboard.sys"
-//  %P       Com port number                       "1"
-//  %R       door full pathname                    "c:\wwiv\temp\door.sys"
-//  %S       Com port BPS rate                     "38400"
-//  %T       Time remaining (min)                  "30"
-
 /**
- * @todo Document this
- */
-
-const string stuff_in(const string& commandline, const string& arg1,
-                      const string& arg2, const string& arg3,
-                      const string& arg4, const string& arg5) {
-  vector<string> flags = { arg1, arg2, arg3, arg4, arg5 };
+ * Creates a commandline by expanding the replacable parmeters offered by
+ * WWIV.
+ * 
+ * If you add one, please update theese docs:
+ * - http://docs.wwivbbs.org/en/latest/chains/parameters
+ * - bbs/admin/gfiles/cmdhelp.msg for the event editor.
+ * 
+ * Replacable parameters:
+ * ~~~~~~~~~~~~~~~~~~~~~~
+ *  Param    Description                       Example
+ *  ----------------------------------------------------------------------------
+ *  %%       A single '%'                          "%"
+ *  %1-%5    Specified passed-in parameter
+ *  %A       callinfo full pathname                "c:\wwiv\temp\callinfo.bbs"
+ *  %C       chain.txt full pathname               "c:\wwiv\temp\chain.txt"
+ *  %D       doorinfo full pathname                "c:\wwiv\temp\dorinfo1.def"
+ *  %E       door32.sys full pathname              "C:\wwiv\temp\door32.sys"
+ *  %H       Socket Handle                         "1234"
+ *  %I       Full Path to TEMP instance directory  "C:\wwiv\temp"
+ *  %K       gfiles comment file for archives      "c:\wwiv\gfiles\comment.txt"
+ *  %M       Modem BPS rate                         "38400"
+ *  %N       Instance number                       "1"
+ *  %O       pcboard full pathname                 "c:\wwiv\temp\pcboard.sys"
+ *  %P       Com port number                       "1"
+ *  %R       door full pathname                    "c:\wwiv\temp\door.sys"
+ *  %S       Com port BPS rate                     "38400"
+ *  %T       Time remaining (min)                  "30"
+ *  %U       Username                              "Rushfan #1"
+*/
+const string stuff_in(const string& commandline, const string& arg1, const string& arg2,
+                      const string& arg3, const string& arg4, const string& arg5) {
+  vector<string> flags = {arg1, arg2, arg3, arg4, arg5};
 
   auto iter = begin(commandline);
   std::ostringstream os;
@@ -79,7 +81,9 @@ const string stuff_in(const string& commandline, const string& arg1,
       char ch = to_upper_case<char>(*iter);
       switch (ch) {
       // fixed strings
-      case '%': os << "%"; break;
+      case '%':
+        os << "%";
+        break;
       // replacable parameters
       case '1':
       case '2':
@@ -89,22 +93,52 @@ const string stuff_in(const string& commandline, const string& arg1,
         os << flags.at(ch - '1');
         break;
       // call-specific numbers
-      case 'A': os << create_filename(CHAINFILE_CALLINFO); break;
-      case 'C': os << create_filename(CHAINFILE_CHAIN); break;
-      case 'D': os << create_filename(CHAINFILE_DORINFO); break;
-      case 'E': os << create_filename(CHAINFILE_DOOR32); break;
-      case 'H': os << a()->remoteIO()->GetDoorHandle(); break;
-      case 'I': os << a()->temp_directory(); break;
-      case 'K': os << a()->config()->gfilesdir() << COMMENT_TXT; break;
-      case 'M': os << a()->modem_speed_; break;
-      case 'N': os << a()->instance_number(); break;
-      case 'O': os << create_filename(CHAINFILE_PCBOARD); break;
-      case 'P': os << ((incom) ? a()->primary_port() : 0); break;
-      case 'R': os << create_filename(CHAINFILE_DOOR); break;
+      case 'A':
+        os << create_filename(CHAINFILE_CALLINFO);
+        break;
+      case 'C':
+        os << create_filename(CHAINFILE_CHAIN);
+        break;
+      case 'D':
+        os << create_filename(CHAINFILE_DORINFO);
+        break;
+      case 'E':
+        os << create_filename(CHAINFILE_DOOR32);
+        break;
+      case 'H':
+        os << a()->remoteIO()->GetDoorHandle();
+        break;
+      case 'I':
+        os << a()->temp_directory();
+        break;
+      case 'K':
+        os << a()->config()->gfilesdir() << COMMENT_TXT;
+        break;
+      case 'M':
+        os << a()->modem_speed_;
+        break;
+      case 'N':
+        os << a()->instance_number();
+        break;
+      case 'O':
+        os << create_filename(CHAINFILE_PCBOARD);
+        break;
+      case 'P':
+        os << ((incom) ? a()->primary_port() : 0);
+        break;
+      case 'R':
+        os << create_filename(CHAINFILE_DOOR);
+        break;
       // TODO(rushfan): Should we deprecate this? Use modem speed for now.
-      case 'S': os << a()->modem_speed_; break;
-      case 'T': os << GetTimeLeft(); break;
-      case 'U': os << a()->user()->GetName(); break;
+      case 'S':
+        os << a()->modem_speed_;
+        break;
+      case 'T':
+        os << GetTimeLeft();
+        break;
+      case 'U':
+        os << a()->user()->GetName();
+        break;
       }
       ++iter;
     } else {
