6081157063c335071262f78ea8d8038040c302d0
rushfan <rushfan@wwivbbs.org>
2019-09-04 18:01:17 -0700

    Don't use %t with gcc
    Apparently it doesn't work, we actually don't need to
    use it since we can be more strict with the dateformat
    and only accept a single space.

--- core/datetime.cpp
+++ core/datetime.cpp
@@ -189,7 +189,7 @@ DateTime parse_yyyymmdd_with_optional_hms(const std::string& date_str) {
   std::istringstream ss{date_str};
   ss.exceptions(std::ios::goodbit);
   std::tm dt = {};
-  ss >> std::get_time(&dt, "%Y-%m-%d%t%H:%M:%S");
+  ss >> std::get_time(&dt, "%Y-%m-%d %H:%M:%S");
   if (ss.fail()) {
     return parse_yyyymmdd(date_str);
   }
