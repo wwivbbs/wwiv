5fa6439f50d58058ce74072f1a0e60a98dfcf3eb
Rushfan <rushfan@wwivbbs.org>
2020-05-17 08:35:56 -0700

    Update debug.sh to autofind the repo location.
    This makes it easier to invoke from not only a subdirectory
    of the git repo, but from another directory altogether which
    makes less clutter in "git status" output and makes it less
    exclusions that have to happen in vscode.

--- debug.sh
+++ debug.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
-declare -r OS=$(uname)
 
-if [[ "${OS}" == "SunOS" ]]; then
-    echo "Setting compiler to gcc for SunOS"
-    export CXX=/usr/bin/g++
-    export CC=/usr/bin/gcc
+# WWIV Version 5.x
+# Copyright (C)2014-2020, WWIV Software Services
+#
+# Licensed  under the  Apache License, Version  2.0 (the "License");
+# you may not use this  file  except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless  required  by  applicable  law  or agreed to  in  writing,
+# software  distributed  under  the  License  is  distributed on an
+# AS IS"  BASIS, WITHOUT  WARRANTIES  OR  CONDITIONS OF ANY  KIND,
+# either  express  or implied.  See  the  License for  the specific
+# language governing permissions and limitations under the License.
+#
+
+declare -r OS=$(uname)
+declare -r HERE=$(dirname $(realpath $0))
+declare -r NINJA=$(which ninja)
+
+if ! [ -x "$(command -v cmake)" ]; then
+	echo "** ERROR: Please install cmake"
+	exit 1
 fi
 
-declare -r NINJA=$(which ninja)
-if [[ -x "${NINJA}" ]]; then
-	echo "Using Ninja Build Tool: ${NINJA}"
-else
+if ! [ -x "$(command -v ninja)" ]; then
 	echo "** ERROR: Please install ninja"
 	echo ""
 	echo "   Debian: apt install ninja-build"
@@ -18,4 +32,13 @@ else
 	exit 1
 fi
 
-cmake -DCMAKE_BUILD_TYPE:STRING=Debug -G "Ninja" $@
+if [[ "${OS}" == "SunOS" ]]; then
+    echo "Setting compiler to gcc for SunOS"
+    export CXX=/usr/bin/g++
+    export CC=/usr/bin/gcc
+fi
+
+echo "Using Ninja Build Tool: ${NINJA}"
+echo "Using Source Root:      ${HERE}"
+
+cmake -DCMAKE_BUILD_TYPE:STRING=Debug -G "Ninja" ${HERE}
