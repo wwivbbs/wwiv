6cc07d0c0a9e758d39c233d26449f6b737366aa6
Rushfan <rushfan@wwivbbs.org>
2020-06-18 23:02:20 -0700

    added "wwivutil files tic validate" command
    Signed-off-by: Rushfan <rushfan@wwivbbs.org>

---
+++ wwivutil/files/tic.cpp
@@ -0,0 +1,91 @@
+/**************************************************************************/
+/*                                                                        */
+/*                          WWIV Version 5.x                              */
+/*             Copyright (C)2018-2020, WWIV Software Services             */
+/*                                                                        */
+/*    Licensed  under the  Apache License, Version  2.0 (the "License");  */
+/*    you may not use this  file  except in compliance with the License.  */
+/*    You may obtain a copy of the License at                             */
+/*                                                                        */
+/*                http://www.apache.org/licenses/LICENSE-2.0              */
+/*                                                                        */
+/*    Unless  required  by  applicable  law  or agreed to  in  writing,   */
+/*    software  distributed  under  the  License  is  distributed on an   */
+/*    "AS IS"  BASIS, WITHOUT  WARRANTIES  OR  CONDITIONS OF ANY  KIND,   */
+/*    either  express  or implied.  See  the  License for  the specific   */
+/*    language governing permissions and limitations under the License.   */
+/**************************************************************************/
+#include "sdk/files/tic.h"
+#include "wwivutil/files/tic.h"
+
+#include "core/log.h"
+#include "core/strings.h"
+#include "sdk/config.h"
+#include <iostream>
+#include <map>
+#include <string>
+
+using std::cout;
+using std::endl;
+using std::map;
+using std::string;
+using namespace wwiv::core;
+using namespace wwiv::sdk;
+using namespace wwiv::strings;
+
+namespace wwiv::wwivutil::files {
+
+
+class TicValidateCommand : public UtilCommand {
+public:
+  TicValidateCommand() : UtilCommand("validate", "Validates the contents of a TIC file.") {}
+
+  virtual ~TicValidateCommand() = default;
+
+  [[nodiscard]] std::string GetUsage() const override final {
+    std::ostringstream ss;
+    ss << "Usage:   list [search spec]" << endl;
+    return ss.str();
+  }
+
+  int Execute() override final {
+    if (remaining().empty()) {
+      std::cerr << "missing filename";
+      return 2;
+    }
+    const std::filesystem::path fn{remaining().front()};
+    const auto dir = fn.parent_path();
+    sdk::files::TicParser parser(dir);
+    const auto otic = parser.parse(fn.filename().string());
+    if (!otic) {
+      LOG(ERROR) << "Failed to parse TIC file: " << fn.string();
+    }
+    const auto tic = otic.value();
+    if (!tic.IsValid()) {
+      LOG(INFO) << "crc_valid: " << tic.crc_valid();
+      LOG(INFO) << "size_valid: " << tic.size_valid();
+      LOG(INFO) << "Exists:     " << tic.exists();
+      LOG(INFO) << "IsValid:    " << tic.IsValid();
+      return 1;
+    }
+    LOG(INFO) << "IsValid:    " << tic.IsValid();
+    return 0;
+  }
+
+  bool AddSubCommands() override final { return true; }
+};
+
+std::string TicCommand::GetUsage() const {
+  std::ostringstream ss;
+  ss << "Usage:   allow " << endl;
+  return ss.str();
+}
+
+bool TicCommand::AddSubCommands() {
+  if (!add(std::make_unique<TicValidateCommand>())) {
+    return false;
+  }
+  return true;
+}
+
+} // namespace wwiv
---
+++ wwivutil/files/tic.h
@@ -0,0 +1,36 @@
+/**************************************************************************/
+/*                                                                        */
+/*                              WWIV Version 5.x                          */
+/*             Copyright (C)2018-2020, WWIV Software Services             */
+/*                                                                        */
+/*    Licensed  under the  Apache License, Version  2.0 (the "License");  */
+/*    you may not use this  file  except in compliance with the License.  */
+/*    You may obtain a copy of the License at                             */
+/*                                                                        */
+/*                http://www.apache.org/licenses/LICENSE-2.0              */
+/*                                                                        */
+/*    Unless required  by  applicable  law  or agreed to  in writing,     */
+/*    software  distributed  under  the  License  is  distributed on an   */
+/*    "AS IS"  BASIS, WITHOUT  WARRANTIES  OR  CONDITIONS OF ANY  KIND,   */
+/*    either  express  or implied.  See  the  License for  the specific   */
+/*    language governing permissions and limitations under the License.   */
+/**************************************************************************/
+#ifndef __INCLUDED_WWIVUTIL_FILES_TIC_H__
+#define __INCLUDED_WWIVUTIL_FILES_TIC_H__
+
+#include "wwivutil/command.h"
+#include <string>
+
+namespace wwiv::wwivutil::files {
+
+class TicCommand final: public UtilCommand {
+public:
+  TicCommand() : UtilCommand("tic", "Manipulate TIC files") {}
+  [[nodiscard]] std::string GetUsage() const override final;
+  bool AddSubCommands() override final;
+};
+
+
+} // namespace wwiv
+
+#endif  // __INCLUDED_WWIVUTIL_FILES_ALLOW_H__
