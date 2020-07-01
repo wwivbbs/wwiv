f11dbaa455fff6381dd783b5ae5b45b627cd83f0
Rushfan <rushfan@wwivbbs.org>
2020-06-30 22:27:02 -0700

    addtest for block chars

--- sdk_test/files/diz_test.cpp
+++ sdk_test/files/diz_test.cpp
@@ -16,15 +16,23 @@
 /*    language governing permissions and limitations under the License.   */
 /*                                                                        */
 /**************************************************************************/
+#include "core/log.h"
+#include "core/strings.h"
+
+
 #include "gtest/gtest.h"
 
 #include "core_test/file_helper.h"
 #include "sdk/files/diz.h"
 
-
-TEST(DizTest, Smoke) {
+class DizTest : public testing::Test {
+public:
+  DizTest() {
+  }
   FileHelper helper;
+};
 
+TEST_F(DizTest, Smoke) {
   const std::string kDIZ = R"(Line1
 Line2
 Line3)";
@@ -40,9 +48,7 @@ Line3)";
   EXPECT_EQ(d.extended_description(), "Line2\nLine3\n");
 }
 
-TEST(DizTest, Smoke_NotDescription) {
-  FileHelper helper;
-
+TEST_F(DizTest, Smoke_NotDescription) {
   const std::string kDIZ = R"(Line1
 Line2
 Line3)";
@@ -56,4 +62,30 @@ Line3)";
   auto d = o.value();
   EXPECT_EQ(d.description(), "");
   EXPECT_EQ(d.extended_description(), "Line1\nLine2\nLine3\n");
+}
+
+TEST_F(DizTest, BlockChars) {
+  const char* kTEXT = R"(ÚÄÄÄÄÄÄÄÄÄÄÄ[ CyberWall v2.0áL ]ÄÄÄÄÄÄÄÄÄÄÄ¿
+³ Smaller  version  of  the  popular  door ³
+³ CyberWall.   Simple  and  easy  to  use, ³
+³ really  cool ANSi. Not  as many features ³
+³ as  the  full  version,  but  this  is a ³
+³ freeware copy if you don't wanna pay for ³
+³ the   full   version.   Freeware   Copy. ³
+³ÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄ³
+³        --- LiQUID pRODUCTIoNS ---        ³
+³ MegaStuff BBS % (317) 873-5173 % 8am-5pm ³
+ÀÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÙ)"; 
+
+  const auto file = helper.CreateTempFile("file_id.diz", kTEXT);
+
+  wwiv::sdk::files::DizParser p(true);
+  auto o = p.parse(file);
+  ASSERT_TRUE(o);
+
+  auto d = o.value();
+  EXPECT_EQ(d.description(), "[ CyberWall v2.0áL ]");
+
+  auto v = wwiv::strings::SplitString(d.extended_description(), "\n");
+  EXPECT_EQ(10u, v.size());
 }
