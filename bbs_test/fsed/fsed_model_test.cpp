288ece1c09874c5ad87fe669ed4d7d561f07bbb9
Rushfan <rushfan@wwivbbs.org>
2020-07-27 21:07:18 -0700

    some simple unit tests for the editor

--- bbs_test/fsed/fsed_model_test.cpp
+++ bbs_test/fsed/fsed_model_test.cpp
@@ -21,6 +21,7 @@
 #include "bbs/fsed/model.h"
 #include "bbs_test/bbs_helper.h"
 #include "core/strings.h"
+#include "core/stl.h"
 #include <iostream>
 #include <memory>
 #include <string>
@@ -28,10 +29,33 @@
 using std::cout;
 using std::endl;
 using std::string;
+using namespace wwiv::stl;
+
+class FakeView : public wwiv::bbs::fsed::editor_viewport_t {
+public:
+  // Inherited via editor_viewport_t
+  int max_view_lines() const override { return 10; }
+  int max_view_columns() const override { return 20; }
+  int top_line() const override { return top_line_; }
+  void set_top_line(int l) override { top_line_ = l; }
+  void gotoxy(const wwiv::bbs::fsed::FsedModel& ed) override { 
+    x_ = ed.cx;
+    y_ = ed.cy;
+  }
+
+  int top_line_{0};
+  int x_{0};
+  int y_{0};
+};
 
 class FsedModelTest : public ::testing::Test {
 protected:
-  int Puts(string s) {
+  FsedModelTest() { 
+    view = std::make_shared<FakeView>();
+    ed.set_view(view);
+  }
+
+  void Add(string s) {
     for (const auto& c : s) {
       if (c == '\n') {
         ed.enter();
@@ -40,7 +64,15 @@ protected:
       ed.add(c);
     }
   }
+
+  void AddTestLines(int n) {
+    for (int i = 0; i < n; i++) {
+      Add(fmt::format("Test-{}\n", i));
+    }
+  }
+
   wwiv::bbs::fsed::FsedModel ed{255};
+  std::shared_ptr<FakeView> view;
 };
 
 TEST_F(FsedModelTest, AddSingle_Character) {
@@ -49,4 +81,50 @@ TEST_F(FsedModelTest, AddSingle_Character) {
   EXPECT_EQ(1, ed.cx);
   EXPECT_EQ(0, ed.curli);
   EXPECT_EQ(1, wwiv::stl::ssize(ed));
+}
+
+TEST_F(FsedModelTest, AddSingleline) {
+  const std::string line = "Hello World";
+  Add(line);
+  EXPECT_EQ(0, ed.cy);
+  EXPECT_EQ(ssize(line), ed.cx);
+  EXPECT_EQ(0, ed.curli);
+  EXPECT_EQ(1, wwiv::stl::ssize(ed));
+}
+
+TEST_F(FsedModelTest, AddTwolines) {
+  const std::string line = "Hello\nWorld";
+  Add(line);
+  EXPECT_EQ(ssize(std::string("World")), ed.cx);
+  EXPECT_EQ(1, ed.curli);
+  EXPECT_EQ(2, wwiv::stl::ssize(ed));
+  EXPECT_EQ(1, ed.cy);
+}
+
+TEST_F(FsedModelTest, SixLines) {
+  const std::string line = "Hello\nWorld\nThis\nIs\nA\nTest\n";
+  Add(line);
+  EXPECT_EQ(6, ed.curli);
+  EXPECT_EQ(7, wwiv::stl::ssize(ed));
+  EXPECT_EQ(6, ed.cy);
+}
+
+TEST_F(FsedModelTest, PageUp_NoScroll) {
+  const std::string line = "Hello\nWorld\nThis\nIs\nA\nTest\n";
+  Add(line);
+  ed.cursor_pgup();
+  EXPECT_EQ(0, ed.curli);
+  EXPECT_EQ(7, wwiv::stl::ssize(ed));
+  EXPECT_EQ(0, ed.cy);
+}
+
+TEST_F(FsedModelTest, PageUp_Scroll) {
+  AddTestLines(100);
+  EXPECT_EQ(100, ed.curli);
+  EXPECT_EQ(9, ed.cy);
+
+  // Scroll up
+  ed.cursor_pgup();
+  EXPECT_EQ(0, ed.cy);
+  EXPECT_EQ(90, ed.curli);
 }
