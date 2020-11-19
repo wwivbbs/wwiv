ca77456d66ee5d7c5b2aa4721493ba03282ff420
Rushfan <rushfan@wwivbbs.org>
2020-11-19 04:11:55 -0800

    added missed files

---
+++ bbs/pipe_expr.h
@@ -0,0 +1,30 @@
+/**************************************************************************/
+/*                                                                        */
+/*                              WWIV Version 5.x                          */
+/*                  Copyright (C)2020, WWIV Software Services             */
+/*                                                                        */
+/*    Licensed  under the  Apache License, Version  2.0 (the "License");  */
+/*    you may not use this  file  except in compliance with the License.  */
+/*    You may obtain a copy of the License at                             */
+/*                                                                        */
+/*                http://www.apache.org/licenses/LICENSE-2.0              */
+/*                                                                        */
+/*    Unless  required  by  applicable  law  or agreed to  in  writing,   */
+/*    software  distributed  under  the  License  is  distributed on an   */
+/*    "AS IS"  BASIS, WITHOUT  WARRANTIES  OR  CONDITIONS OF ANY  KIND,   */
+/*    either  express  or implied.  See  the  License for  the specific   */
+/*    language governing permissions and limitations under the License.   */
+/**************************************************************************/
+#ifndef INCLUDED_BBS_PIPE_EXPR_H
+#define INCLUDED_BBS_PIPE_EXPR_H
+
+#include "common/context.h"
+#include "common/macro_context.h"
+#include "sdk/ansi/ansi.h"
+#include <string>
+namespace wwiv::bbs {
+
+std::string evaluate_pipe_expression(std::string);
+
+}
+#endif
