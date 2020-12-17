aef281071cb97a6fbf07c506c15c30ac451286fa
Rushfan <rushfan@wwivbbs.org>
2020-12-17 07:30:47 -0800

    added cereal config

---
+++ sdk/config_cereal.h
@@ -0,0 +1,123 @@
+/**************************************************************************/
+/*                                                                        */
+/*                              WWIV Version 5.x                          */
+/*             Copyright (C)1998-2020, WWIV Software Services             */
+/*                                                                        */
+/*    Licensed  under the  Apache License, Version  2.0 (the "License");  */
+/*    you may not use this  file  except in compliance with the License.  */
+/*    You may obtain a copy of the License at                             */
+/*                                                                        */
+/*                http://www.apache.org/licenses/LICENSE-2.0              */
+/*                                                                        */
+/*    Unless  required  by  applicable  law  or agreed to  in  writing,   */
+/*    software  distributed  under  the  License  is  distributed on an   */
+/*    "AS IS"  BASIS, WITHOUT  WARRANTIES  OR  CONDITIONS OF ANY  KIND,   */
+/*    either  express  or implied.  See  the  License for  the specific   */
+/*    language governing permissions and limitations under the License.   */
+/*                                                                        */
+/**************************************************************************/
+
+#ifndef INCLUDED_SDK_CONFIG_CEREAL_H
+#define INCLUDED_SDK_CONFIG_CEREAL_H
+
+#include "core/cereal_utils.h"
+#include <cereal/specialize.hpp>
+#include "sdk/config.h"
+
+using namespace wwiv::sdk;
+
+//// We want to override how we store some enums as a string, not int.
+//// This has to be in the global namespace.
+//CEREAL_SPECIALIZE_FOR_ALL_ARCHIVES(chain_exec_mode_t, specialization::non_member_load_save_minimal);
+//CEREAL_SPECIALIZE_FOR_ALL_ARCHIVES(chain_exec_dir_t, specialization::non_member_load_save_minimal);
+
+namespace cereal {
+
+//template <class Archive>
+//std::string save_minimal(Archive const&, const chain_exec_mode_t& t) {
+//  return to_enum_string<chain_exec_mode_t>(t, {"none", "DOS", "FOSSIL", "STDIO", "NETFOSS"});
+//}
+//template <class Archive>
+//void load_minimal(Archive const&, chain_exec_mode_t& t, const std::string& s) {
+//  t = from_enum_string<chain_exec_mode_t>(s, {"none", "DOS", "FOSSIL", "STDIO", "NETFOSS"});
+//}
+
+template <class Archive> void serialize(Archive& ar, config_header_t& n) {
+  SERIALIZE(n, config_revision_number);
+  SERIALIZE(n, last_written_date);
+  SERIALIZE(n, written_by_wwiv_num_version);
+  SERIALIZE(n, written_by_wwiv_version);
+}
+template <class Archive> void serialize(Archive& ar, slrec& n) {
+  SERIALIZE(n, time_per_day);
+  SERIALIZE(n, time_per_logon);
+  SERIALIZE(n, messages_read);
+  SERIALIZE(n, emails);
+  SERIALIZE(n, posts);
+  SERIALIZE(n, ability); // todo: change to booleans?
+}
+
+template <class Archive> void serialize(Archive& ar, valrec& n) {
+  SERIALIZE(n, sl);
+  SERIALIZE(n, dsl);
+  SERIALIZE(n, ar);
+  SERIALIZE(n, dar);
+  SERIALIZE(n, restrict);
+}
+
+
+template <class Archive> void serialize(Archive& ar, config_t& n) {
+  SERIALIZE(n, header);
+  SERIALIZE(n, systempw);
+  SERIALIZE(n, msgsdir);
+  SERIALIZE(n, gfilesdir);
+  SERIALIZE(n, datadir);
+  SERIALIZE(n, dloadsdir);
+  SERIALIZE(n, scriptdir);
+  SERIALIZE(n, logdir);
+  SERIALIZE(n, systemname);
+  SERIALIZE(n, systemphone);
+  SERIALIZE(n, sysopname);
+  SERIALIZE(n, menudir);
+
+  SERIALIZE(n, closedsystem);
+
+  SERIALIZE(n, newuserpw);
+  SERIALIZE(n, newusersl);
+  SERIALIZE(n, newuserdsl);
+  SERIALIZE(n, newusergold);
+  SERIALIZE(n, newuser_restrict);
+
+  SERIALIZE(n, newuploads);
+  SERIALIZE(n, sysconfig);
+  SERIALIZE(n, sysoplowtime);
+  SERIALIZE(n, sysophightime);
+  SERIALIZE(n, wwiv_reg_number);
+  SERIALIZE(n, post_call_ratio);
+  SERIALIZE(n, req_ratio);
+
+  SERIALIZE(n, sl);
+  SERIALIZE(n, autoval);
+
+  SERIALIZE(n, userreclen);
+  SERIALIZE(n, waitingoffset);
+  SERIALIZE(n, inactoffset);
+  SERIALIZE(n, sysstatusoffset);
+  SERIALIZE(n, fsoffset);
+  SERIALIZE(n, fuoffset);
+  SERIALIZE(n, fnoffset);
+  SERIALIZE(n, qscn_len);
+
+  SERIALIZE(n, maxwaiting);
+  SERIALIZE(n, maxusers);
+  SERIALIZE(n, max_subs);
+  SERIALIZE(n, max_dirs);
+  SERIALIZE(n, max_backups);
+  SERIALIZE(n, script_flags);
+}
+
+
+} // namespace cereal
+
+
+#endif
