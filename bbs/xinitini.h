02dd19d49b5106dae01146f7652f8857575c9fe9
rushfan <rushfan@wwivbbs.org>
2021-02-16 07:22:41 -0800

    removed WWIV.INI string and sysconfig flag for WFC_SCREEN and NEWUSER_MIN

--- bbs/xinitini.h
+++ bbs/xinitini.h
@@ -17,8 +17,8 @@
 /*                                                                        */
 /**************************************************************************/
 
-#ifndef __INCLUDED_XINITINI_H__
-#define __INCLUDED_XINITINI_H__
+#ifndef INCLUDED_BBS_XINITINI_H
+#define INCLUDED_BBS_XINITINI_H
 
 //---SYSTEM OPTIONS--
 constexpr const char* INI_STR_SPAWNOPT = "SPAWNOPT";
@@ -69,7 +69,7 @@ constexpr const char* INI_STR_TWO_COLOR_CHAT = "TWO_COLOR_CHAT";
 constexpr const char* INI_STR_ALLOW_ALIASES = "ALLOW_ALIASES";
 constexpr const char* INI_STR_FREE_PHONE = "FREE_PHONE";
 constexpr const char* INI_STR_EXTENDED_USERINFO = "EXTENDED_USERINFO";
-constexpr const char* INI_STR_NEWUSER_MIN = "NEWUSER_MIN";
+//constexpr const char* INI_STR_NEWUSER_MIN = "NEWUSER_MIN";
 constexpr const char* INI_STR_ALLOW_CC_BCC = "ALLOW_CC_BCC";
 constexpr const char* INI_STR_MAIL_WHO_LEN = "MAIL_WHO_LEN";
 constexpr const char* INI_STR_ATTACH_DIR = "ATTACH_DIR";
@@ -83,7 +83,7 @@ constexpr const char* INI_STR_MAX_CHAINS = "MAX_CHAINS";
 constexpr const char* INI_STR_MAX_GFILESEC = "MAX_GFILESEC";
 
 //---CALLOUT/WFC--
-constexpr const char* INI_STR_WFC_SCREEN = "WFC_SCREEN";
+//constexpr const char* INI_STR_WFC_SCREEN = "WFC_SCREEN";
 constexpr const char* INI_STR_SCREEN_SAVER_TIME = "SCREEN_SAVER_TIME";
 
 //---ASV SETTINGS---
@@ -101,4 +101,4 @@ constexpr const char* INI_STR_EXEC_LOG_SYNCFOSS = "EXEC_LOG_SYNCFOSS";
 constexpr const char* INI_STR_EXEC_CHILD_WAIT_TIME =   "EXEC_CHILD_WAIT_TIME";
 constexpr const char* INI_STR_NEW_SCAN_AT_LOGIN  = "NEW_SCAN_AT_LOGIN";
 
-#endif  // __INCLUDED_XINITINI_H__
+#endif  // INCLUDED_BBS_XINITINI_H
