09b7a235d32ba3d049f8c499fe9891a8bffc8b77
rushfan <rushfan@wwivbbs.org>
2021-03-09 21:25:43 -0800

    added tests for pipe and heart filter

--- sdk_test/ansi/ansi_test.cpp
+++ sdk_test/ansi/ansi_test.cpp
@@ -16,13 +16,13 @@
 /*    language governing permissions and limitations under the License.   */
 /*                                                                        */
 /**************************************************************************/
-#include "gtest/gtest.h"
-
 #include "core/stl.h"
 #include "core/strings.h"
 #include "sdk/ansi/ansi.h"
 #include "sdk/ansi/framebuffer.h"
 #include "sdk_test/sdk_helper.h"
+
+#include "gtest/gtest.h"
 #include <string>
 
 using namespace wwiv::strings;
@@ -30,14 +30,26 @@ using namespace wwiv::sdk::ansi;
 
 class AnsiTest : public testing::Test {
 public:
-  AnsiTest() : b(10), ansi(&b, {} , 0x07) {}
+  AnsiTest()
+      : b(10), ansi(&b, {}, 0x07), heart_and_pipe(&ansi, {7, 11, 14, 13, 31, 10, 12, 9, 5, 3}) {}
 
   void write(const std::string& s) {
     ansi.write(s);
     b.close();
   }
 
-  void check(const std::vector<std::string>& lines) {
+  bool write_hp(const std::string& s) {
+    auto result = true;
+    for (const auto c : s) {
+      if (!heart_and_pipe.write(c)) {
+        result = false;
+      }
+    }
+    b.close();
+    return result;
+  }
+
+  void check(const std::vector<std::string>& lines) const {
     EXPECT_EQ(wwiv::stl::ssize(lines), b.rows());
     for (std::vector<std::string>::size_type i = 0; i < lines.size(); i++) {
       EXPECT_EQ(lines[i], b.row_as_text(i)) << "Line #" << i;
@@ -46,6 +58,7 @@ public:
 
   FrameBuffer b;
   Ansi ansi;
+  HeartAndPipeCodeFilter heart_and_pipe;
 };
 
 TEST_F(AnsiTest, ColorBlue) {
@@ -95,4 +108,22 @@ TEST_F(AnsiTest, CSI_D_Left_TooFar) {
 TEST_F(AnsiTest, CSI_K_ClearEOL) {
   write("Hello\nWorld\n\x1b[2A\x1b[4C\x1b[K");
   check({"Hell      ", "World"});
+}
+
+TEST_F(AnsiTest, HeartAndPipe_HeartSmoke) {
+  write_hp("\x03""1Hello");
+  check({"Hello"});
+  EXPECT_EQ(11, b.curatr());
+}
+
+TEST_F(AnsiTest, HeartAndPipe_StdPipe) {
+  write_hp("|12Hello");
+  check({"Hello"});
+  EXPECT_EQ(12, b.curatr());
+}
+
+TEST_F(AnsiTest, HeartAndPipe_WWIVPipe) {
+  write_hp("|#5Hello");
+  check({"Hello"});
+  EXPECT_EQ(10, b.curatr());
 }
