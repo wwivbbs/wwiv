4fe365cbf02535732ee5b4e95b0b02f0657345c3
rushfan <rushfan@wwivbbs.org>
2021-04-23 07:39:12 -0700

    bump version to 5.7.1

--- cmake/Common.cmake
+++ cmake/Common.cmake
@@ -16,7 +16,7 @@ option(WWIV_INSTALL "Create install packages for both zip files and binaries." O
 #
 # Build variables to come from Jenkins, environment, etc.
 
-set(WWIV_RELEASE "5.7.0" CACHE STRING "WWIV Release Major Version to 3 digits")
+set(WWIV_RELEASE "5.7.1" CACHE STRING "WWIV Release Major Version to 3 digits")
 set(WWIV_BUILD_NUMBER "development" CACHE STRING "WWIV Build Number")
 set(WWIV_FULL_RELEASE "${WWIV_RELEASE}.${WWIV_BUILD_NUMBER}" CACHE INTERNAL "WWIV Release Version, 4 digits with build number as 4th")
 set(WWIV_ARCH "x86" CACHE STRING "x86 or x64")
