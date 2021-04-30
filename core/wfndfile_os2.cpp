7e43c4939d61a043b1ae05b57d80841ef64a7007
rushfan <rushfan@wwivbbs.org>
2021-04-29 22:59:23 -0700

    add scandir impl

--- core/wfndfile_os2.cpp
+++ core/wfndfile_os2.cpp
@@ -25,6 +25,7 @@
 #include "core/log.h"
 #include "core/strings.h"
 #include <dirent.h>
+#include <errno.h>
 #include <fnmatch.h>
 #include <iostream>
 #include <libgen.h>
@@ -78,6 +79,93 @@ static int fname_ok(struct dirent* ent) {
     return 1;
   }
   return 0;
+}
+
+int scandir(const char* dir, struct dirent*** namelist, int (*sel)(/* const */ struct dirent*),
+            int (*compare)(const /* struct dirent ** */ void*,
+                           const /* struct dirent ** */ void*)) {
+  struct dirent** list = nullptr;
+  size_t list_size = 0;
+  int list_count = 0;
+  struct dirent* d;
+  int saved_errno;
+
+  auto* dp = opendir(dir);
+  if (dp == NULL) {
+    return -1;
+  }
+
+  /* Save original errno */
+  saved_errno = errno;
+  errno = 0;
+
+  while ((d = readdir(dp)) != nullptr) {
+    int selected = !sel || sel(d);
+    errno = 0;
+
+    if (selected) {
+      struct dirent* d1;
+      size_t len;
+
+      /* List full ? */
+      if (list_count == list_size) {
+        struct dirent** new_list;
+
+        if (list_size == 0)
+          list_size = 20;
+        else
+          list_size += list_size;
+
+        new_list = (struct dirent**)realloc(list, list_size * sizeof(*list));
+        if (!new_list) {
+          errno = ENOMEM;
+          break;
+        }
+
+        list = new_list;
+      }
+
+      /* On OS/2 kLIBC, d_name is not the last member of struct dirent.
+       * So just allocate as many as the size of struct dirent. */
+      len = sizeof(struct dirent);
+      d1 = (struct dirent*)malloc(len);
+      if (!d1) {
+        errno = ENOMEM;
+
+        break;
+      }
+
+      memcpy(d1, d, len);
+
+      list[list_count++] = d1;
+    }
+  }
+
+  if (errno) {
+    /* Store errno to use later */
+    saved_errno = errno;
+
+    /* Free a directory list */
+    while (list_count > 0) {
+      free(list[--list_count]);
+    }
+    free(list);
+
+    /* Indicate an error */
+    list_count = -1;
+  } else {
+    /* If compare is present, sort */
+    if (compare != NULL) {
+      qsort(list, list_count, sizeof(*list), compare);
+    }
+
+    *namelist = list;
+  }
+
+  /* Ignore error of closedir() */
+  closedir(dp);
+  errno = saved_errno;
+  return list_count;
 }
 
 int alphasort (const void *a, const void *b)
