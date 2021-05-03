64f908cc3608e9e77949601029c517cce380d2c8
Rushfan <rushfan@wwivbbs.org>
2021-05-02 19:43:31 -0500

    update prereqs

---
+++ builds/os2/prereq.cmd
@@ -0,0 +1,4 @@
+@rem echo "Installing prerequistes for Debian/Ubuntu"
+
+yum -y update
+yum -y install cmake gcc watcom-wlink-hll gcc-wlink gcc-wrc gcc-c++ p7zip libc libcx kbuild-make binutils libc-devel libcx-devel
